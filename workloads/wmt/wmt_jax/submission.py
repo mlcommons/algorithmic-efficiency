e619c8cf2a9eb248a4b3065cabd6bb3606278acd
ckt624 <ckt624@gmail.com>
2021-05-20 18:49:42 +0000

    Add wmt_jax/

---
+++ workloads/wmt/wmt_jax/submission.py
@@ -0,0 +1,113 @@
+"""Training algorithm track submission functions for MNIST."""
+import functools
+from typing import Iterator, List, Tuple
+
+from . import config
+from . import train
+from flax import jax_utils
+from flax import optim
+from flax.training import common_utils
+import jax
+import numpy as np
+import spec
+
+
+def get_batch_size(workload_name):
+  batch_sizes = {"wmt_jax": config.config.per_device_batch_size}
+  return batch_sizes[workload_name]
+
+
+def init_optimizer_state(workload: spec.Workload,
+                         model_params: spec.ParameterTree,
+                         model_state: spec.ModelAuxiliaryState,
+                         hyperparameters: spec.Hyperparamters,
+                         rng: spec.RandomState) -> spec.OptimizerState:
+  del model_state
+  del rng
+
+  optimizer_def = optim.Adam(
+      learning_rate=hyperparameters.learning_rate,
+      beta1=1.0 - hyperparameters.one_minus_beta_1,
+      beta2=0.98,
+      eps=hyperparameters.epsilon)
+  optimizer = optimizer_def.create(model_params)
+
+  # Replicate optimizer.
+  optimizer = jax_utils.replicate(optimizer)
+
+  learning_rate_fn = train.create_learning_rate_scheduler(
+      base_learning_rate=hyperparameters.learning_rate,
+      warmup_steps=config.config.warmup_steps)
+
+  # compile multidevice versions of train/eval/predict step and cache init fn.
+  p_train_step = jax.pmap(
+      functools.partial(
+          train.train_step,
+          config=workload.train_config,
+          learning_rate_fn=learning_rate_fn,
+          label_smoothing=config.config.label_smoothing),
+      axis_name="batch",
+      donate_argnums=(0,))  # pytype: disable=wrong-arg-types
+
+  return optimizer, p_train_step
+
+
+def update_params(
+    workload: spec.Workload,
+    current_params: spec.ParameterTree,
+    current_params_types: spec.ParameterTypeTree,
+    model_state: spec.ModelAuxiliaryState,
+    hyperparameters: spec.Hyperparamters,
+    augmented_and_preprocessed_input_batch: spec.Tensor,
+    # This will define the output activation via `output_activation_fn`.
+    loss_type: spec.LossType,
+    optimizer_state: spec.OptimizerState,
+    eval_results: List[Tuple[int, float]],
+    global_step: int,
+    rng: spec.RandomState) -> spec.UpdateReturn:
+  """Return (updated_optimizer_state, updated_params)."""
+  del workload
+  del current_params
+  del current_params_types
+  del eval_results
+  del global_step
+  del model_state
+  del loss_type
+  del hyperparameters
+
+  optimizer, p_train_step = optimizer_state
+  dropout_rngs = jax.random.split(rng, jax.local_device_count())
+  optimizer, _ = p_train_step(
+      optimizer,
+      augmented_and_preprocessed_input_batch,
+      dropout_rng=dropout_rngs)
+
+  return (optimizer, p_train_step), optimizer.target, None
+
+
+# Not allowed to update the model parameters, hyperparameters, global step, or
+# optimzier state.
+def data_selection(workload: spec.Workload,
+                   input_queue: Iterator[Tuple[spec.Tensor, spec.Tensor]],
+                   optimizer_state: spec.OptimizerState,
+                   current_params: spec.ParameterTree,
+                   hyperparameters: spec.Hyperparamters, global_step: int,
+                   rng: spec.RandomState) -> Tuple[spec.Tensor, spec.Tensor]:
+  """Select data from the infinitely repeating, pre-shuffled input queue.
+
+  Each element of the queue is a single training example and label.
+
+  We left out `current_params_types` because we do not believe that it would
+  # be necessary for this function.
+
+  Return a tuple of input label batches.
+  """
+  del optimizer_state
+  del current_params
+  del global_step
+  del rng
+  del hyperparameters
+  del workload
+
+  return common_utils.shard(jax.tree_map(np.asarray, next(input_queue)))
+
---
+++ workloads/wmt/wmt_jax/workload.py
@@ -0,0 +1,160 @@
+"""WMT workload implemented in Jax."""
+import functools
+from typing import Tuple
+
+from . import config
+from . import input_pipeline
+from . import models
+from . import train
+from flax import linen as nn
+import jax
+import jax.numpy as jnp
+import spec
+import tensorflow as tf
+
+
+class WMTWorkload(spec.Workload):
+
+  def __init__(self):
+    self._eval_ds = None
+    self._train_ds = None
+    self._sp_tokenizer = None
+    self.train_config = models.TransformerConfig(
+        vocab_size=config.config.vocab_size,
+        output_vocab_size=config.config.vocab_size,
+        share_embeddings=config.config.share_embeddings,
+        logits_via_embedding=config.config.logits_via_embedding,
+        dtype=jnp.bfloat16 if config.config.use_bfloat16 else jnp.float32,
+        emb_dim=config.config.emb_dim,
+        num_heads=config.config.num_heads,
+        num_layers=config.config.num_layers,
+        qkv_dim=config.config.qkv_dim,
+        mlp_dim=config.config.mlp_dim,
+        max_len=max(config.config.max_target_length,
+                    config.config.max_eval_target_length),
+        dropout_rate=config.config.dropout_rate,
+        attention_dropout_rate=config.config.attention_dropout_rate,
+        deterministic=False,
+        decode=False,
+        kernel_init=nn.initializers.xavier_uniform(),
+        bias_init=nn.initializers.normal(stddev=1e-6))
+    self.eval_config = self.train_config.replace(deterministic=True)
+    self.p_eval_step = jax.pmap(
+        functools.partial(train.eval_step, config=self.eval_config),
+        axis_name="batch")
+
+  def has_reached_goal(self, eval_result: float) -> bool:
+    return eval_result["accuracy"] > 0.5
+
+  def build_input_queue(self, data_rng: jax.random.PRNGKey, split: str,
+                        data_dir: str, batch_size: int):
+    tf.io.gfile.makedirs(config.config.workdir)
+    self._train_ds, self._eval_ds, _, self._sp_tokenizer = input_pipeline.get_wmt_datasets(
+        batch_size=jax.local_device_count() * batch_size,
+        config=config.config,
+        reverse_translation=config.config.reverse_translation,
+        vocab_path=config.config.vocab_path)
+    return iter(self._train_ds)
+
+  @property
+  def param_shapes(self):
+    init_params, _ = self.init_model_fn(jax.random.PRNGKey(0))
+    return jax.tree_map(lambda x: spec.ShapeTuple(x.shape), init_params)
+
+  @property
+  def loss_type(self):
+    return spec.LossType.SOFTMAX_CROSS_ENTROPY
+
+  @property
+  def train_mean(self):
+    return 0.0
+
+  @property
+  def train_stddev(self):
+    return 1.0
+
+  def model_params_types(self):
+    pass
+
+  @property
+  def max_allowed_runtime_sec(self):
+    return 60
+
+  @property
+  def eval_period_time_sec(self):
+    return 10
+
+  # Return whether or not a key in spec.ParameterTree is the output layer
+  # parameters.
+  def is_output_params(self, param_key: spec.ParameterKey) -> bool:
+    pass
+
+  def preprocess_for_train(self, selected_raw_input_batch: spec.Tensor,
+                           train_mean: spec.Tensor, train_stddev: spec.Tensor,
+                           rng: spec.RandomState) -> spec.Tensor:
+    del train_mean
+    del train_stddev
+    del rng
+    return selected_raw_input_batch
+
+  def preprocess_for_eval(self, raw_input_batch: spec.Tensor,
+                          train_mean: spec.Tensor,
+                          train_stddev: spec.Tensor) -> spec.Tensor:
+    del train_mean
+    del train_stddev
+    return raw_input_batch
+
+  def init_model_fn(self, rng: spec.RandomState) -> spec.ModelInitState:
+    rng, init_rng = jax.random.split(rng)
+    input_shape = (config.config.per_device_batch_size,
+                   config.config.max_target_length)
+    target_shape = (config.config.per_device_batch_size,
+                    config.config.max_target_length)
+
+    initial_variables = jax.jit(models.Transformer(self.eval_config).init)(
+        init_rng, jnp.ones(input_shape, jnp.float32),
+        jnp.ones(target_shape, jnp.float32))
+
+    initial_params = initial_variables["params"]
+
+    return initial_params, None
+
+  def model_fn(
+      self, params: spec.ParameterTree,
+      augmented_and_preprocessed_input_batch: spec.Tensor,
+      model_state: spec.ModelAuxiliaryState, mode: spec.ForwardPassMode,
+      rng: spec.RandomState,
+      update_batch_norm: bool) -> Tuple[spec.Tensor, spec.ModelAuxiliaryState]:
+    del model_state
+    del rng
+    del update_batch_norm
+
+    model_config = self.train_config if mode == spec.ForwardPassMode.TRAIN else self.eval_config
+    inputs, targets = augmented_and_preprocessed_input_batch[
+        "inputs"], augmented_and_preprocessed_input_batch["targets"]
+    logits_batch = models.Transformer(model_config).apply({"params": params},
+                                                          inputs, targets)
+
+    return logits_batch, None
+
+  def loss_fn(
+      self,
+      label_batch: spec.Tensor,  # Dense (not one-hot) labels.
+      logits_batch: spec.Tensor) -> spec.Tensor:
+
+    weights = jnp.where(label_batch > 0, 1.0, 0.0)
+    metrics = train.compute_metrics(logits_batch, label_batch, weights)
+
+    return metrics
+
+  def eval_model(self, params: spec.ParameterTree,
+                 model_state: spec.ModelAuxiliaryState, rng: spec.RandomState):
+    """Run a full evaluation of the model."""
+    eval_results = train.evaluate(
+        p_eval_step=self.p_eval_step,
+        target=params,
+        eval_ds=self._eval_ds,
+        num_eval_steps=config.config.num_eval_steps)
+
+    return eval_results
+
