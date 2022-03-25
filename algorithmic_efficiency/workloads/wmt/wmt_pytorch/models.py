3e3514a8461bc3626a694aa971ffdbfe5d3ded01
runame <runa.eschenhagen@student.uni-tuebingen.de>
2022-03-25 12:01:43 +0100

    Adjust positional encoding

--- algorithmic_efficiency/workloads/wmt/wmt_pytorch/models.py
+++ algorithmic_efficiency/workloads/wmt/wmt_pytorch/models.py
@@ -165,11 +165,11 @@ class PositionalEncoding(nn.Module):
     self.dropout = nn.Dropout(p=dropout)
 
     position = torch.arange(max_len).unsqueeze(1)
-    div_term = torch.exp(
-        torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+    scale_factor = -math.log(10000.0) / (d_model // 2 - 1)
+    div_term = torch.exp(torch.arange(d_model // 2) *  scale_factor)
     pe = torch.zeros(1, max_len, d_model)
-    pe[0, :, 0::2] = torch.sin(position * div_term)
-    pe[0, :, 1::2] = torch.cos(position * div_term)
+    pe[0, :, :d_model // 2] = torch.sin(position * div_term)
+    pe[0, :, d_model // 2:2 * (d_model // 2)] = torch.cos(position * div_term)
     self.register_buffer('pe', pe)
 
   def forward(self, x: Tensor, inputs_positions: Tensor) -> Tensor:
