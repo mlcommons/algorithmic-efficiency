8b8ae2b90202fdf7926075d18b95e0db27e7d943
Daniel Snider <danielsnider12@gmail.com>
2022-04-02 13:28:37 -0400

    Skeleton test file

---
+++ algorithmic_efficiency/logging_utils_test.py
@@ -0,0 +1,14 @@
+"""Tests for logging_utils.py."""
+
+from absl.testing import absltest
+from algorithmic_efficiency import logging_utils
+
+
+class LoggingUtilsTest(absltest.TestCase):
+  """Tests for logging_utils.py."""
+
+  def test_logging_utils_mode_min(self):
+    pass
+
+if __name__ == '__main__':
+  absltest.main()
