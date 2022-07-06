9e2ea780dd68e432d9055a6d73be6ca40e78e14e
runame <runa.eschenhagen@student.uni-tuebingen.de>
2022-07-06 18:27:26 +0200

    Sync eval metrics for DDP

--- algorithmic_efficiency/workloads/ogbg/metrics.py
+++ algorithmic_efficiency/workloads/ogbg/metrics.py
@@ -1,5 +1,7 @@
 # Forked from Flax example which can be found here:
 # https://github.com/google/flax/blob/main/examples/ogbg_molpcba/train.py
+import os
+from typing import Any
 
 from clu import metrics
 import flax
@@ -7,6 +9,13 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 from sklearn.metrics import average_precision_score
+import torch
+import torch.distributed as dist
+
+USE_PYTORCH_DDP = 'LOCAL_RANK' in os.environ
+RANK = int(os.environ['LOCAL_RANK']) if USE_PYTORCH_DDP else 0
+DEVICE = torch.device(f'cuda:{RANK}' if torch.cuda.is_available() else 'cpu')
+N_GPUS = torch.cuda.device_count()
 
 
 def predictions_match_labels(*,
@@ -31,6 +40,17 @@ class MeanAveragePrecision(
     logits = values['logits']
     mask = values['mask']
 
+    if USE_PYTORCH_DDP:
+      # Sync labels, logits, and masks across devices.
+      all_values = [labels, logits, mask]
+      for idx, array in enumerate(all_values):
+        tensor = torch.as_tensor(array, device=DEVICE)
+        # Assumes that the tensors on all devices have the same shape.
+        all_tensors = [torch.zeros_like(tensor) for _ in range(N_GPUS)]
+        dist.all_gather(all_tensors, tensor)
+        all_values[idx] = torch.cat(all_tensors).cpu().numpy()
+      labels, logits, mask = all_values
+
     mask = mask.astype(np.bool)
 
     probs = jax.nn.sigmoid(logits)
@@ -52,8 +72,25 @@ class MeanAveragePrecision(
     return np.nanmean(average_precisions)
 
 
+class AverageDDP(metrics.Average):
+  """Supports syncing metrics for PyTorch distributed data parallel (DDP)."""
+
+  def compute(self) -> Any:
+    if USE_PYTORCH_DDP:
+      # Sync counts across devices.
+      total_tensor = torch.as_tensor(np.asarray(self.total), device=DEVICE)
+      count_tensor = torch.as_tensor(np.asarray(self.count), device=DEVICE)
+      dist.all_reduce(total_tensor)
+      dist.all_reduce(count_tensor)
+      # Hacky way to avoid FrozenInstanceError
+      # (https://docs.python.org/3/library/dataclasses.html#frozen-instances).
+      object.__setattr__(self, 'total', total_tensor.cpu().numpy())
+      object.__setattr__(self, 'count', count_tensor.cpu().numpy())
+    return super().compute()
+
+
 @flax.struct.dataclass
 class EvalMetrics(metrics.Collection):
-  accuracy: metrics.Average.from_fun(predictions_match_labels)
-  loss: metrics.Average.from_output('loss')
+  accuracy: AverageDDP.from_fun(predictions_match_labels)
+  loss: AverageDDP.from_output('loss')
   mean_average_precision: MeanAveragePrecision
