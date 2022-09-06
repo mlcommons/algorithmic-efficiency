5f49dc693bf94f1de954f44bb31453af6f560a6c
Sourabh Medapati <smedapati@google.com>
2022-09-06 08:55:01 +0000

    changes for review

--- algorithmic_efficiency/spec.py
+++ algorithmic_efficiency/spec.py
@@ -239,7 +239,7 @@ class Workload(metaclass=abc.ABCMeta):
     """Evaluate the model on a given dataset split, return final scalars."""
   
   def create_summary_writer(self, log_dir):
-      print('tensorboard summaries at {}'.format(log_dir))
+      logging.info('tensorboard summaries at {}'.format(log_dir))
       self.summary_writer = tensorboard.SummaryWriter(log_dir)    
       
   def eval_model(self,
