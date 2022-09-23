3c6f7fbcf53bfd0b504fc222090ef7cf840ac626
Zack Nado <znado@google.com>
2022-09-23 15:35:52 -0400

    init dataset_setup.py

---
+++ dataset_setup.py
@@ -0,0 +1,246 @@
+r"""MLCommons dataset setup script.
+
+If you already have a copy of a dataset(s), you can skip download it and provide
+the path when running your algorithm with submission_runner.py via --data_dir.
+
+Note that in order to avoid potential accidental deletion, this script does NOT
+delete any intermediate temporary files (such as zip archives). All of these
+files will be downloaded to the provided --temp_dir, and the user can manually
+delete these after downloading has finished.
+
+If mounting a GCS bucket with gcsfuse, --temp_dir should NOT be a path to the
+GCS bucket, as this can result in *orders of magnitude* slower download speeds
+due to write speed issues (--dataset_dir can include the GCS bucket though).
+
+Criteo download size: ~350GB
+Criteo temp disk size: ~
+Criteo final disk size:
+FastMRI download size:
+FastMRI final disk size:
+LibriSpeech download size:
+LibriSpeech final disk size:
+OGBG download size:
+OGBG final disk size:
+WMT download size:
+WMT final disk size:
+_______________________
+Total download size:
+Total disk size:
+
+Some datasets require signing a form before downloading:
+
+FastMRI:
+Fill out form on https://fastmri.med.nyu.edu/ and run this script with the
+links that are emailed to you for "knee_singlecoil_train" and
+"knee_singlecoil_val".
+
+ImageNet:
+Register on https://image-net.org/ and run this script with the links to the
+ILSVRC2012 train and validation images.
+
+Note for tfds ImageNet, you may have to increase the max number of files allowed
+open at once using `ulimit -n 8192`.
+
+Example command:
+
+python3 dataset_setup.py --dataset_dir=/data --temp_dir=/tmp/mlcommons_data
+"""
+import os
+
+from absl import app
+from absl import flags
+from absl import logging
+import subprocess
+
+flags.DEFINE_boolean(
+    'all',
+    True,
+    'Whether or not to download all datasets. If false, can download some '
+    'combination of datasets by setting the individual dataset flags below.')
+flags.DEFINE_boolean(
+    'criteo',
+    False,
+    'If --all=false, whether or not to download Criteo.')
+flags.DEFINE_boolean(
+    'fastmri',
+    False,
+    'If --all=false, whether or not to download FastMRI.')
+flags.DEFINE_boolean(
+    'librispeech',
+    False,
+    'If --all=false, whether or not to download LibriSpeech.')
+flags.DEFINE_boolean(
+    'ogbg',
+    False,
+    'If --all=false, whether or not to download OGBG.')
+flags.DEFINE_boolean(
+    'wmt',
+    False,
+    'If --all=false, whether or not to download WMT.')
+
+flags.DEFINE_string(
+    'dataset_dir',
+    None,
+    'The path to the folder where datasets should be downloaded.')
+flags.DEFINE_string(
+    'temp_dir',
+    '/tmp',
+    'A local path to a folder where temp files can be downloaded.')
+
+flags.DEFINE_string(
+    'imagenet_train_url',
+    None,
+    'Only necessary if you want this script to `wget` the ImageNet train '
+    'split. If not, you can supply the path to --data_dir in '
+    'submission_runner.py.')
+flags.DEFINE_string(
+    'imagenet_val_url',
+    None,
+    'Only necessary if you want this script to `wget` the ImageNet validation '
+    'split. If not, you can supply the path to --data_dir in '
+    'submission_runner.py.')
+flags.DEFINE_string(
+    'fastmri_knee_singlecoil_train_url',
+    None,
+    'Only necessary if you want this script to `wget` the FastMRI train '
+    'split. If not, you can supply the path to --data_dir in '
+    'submission_runner.py.')
+flags.DEFINE_string(
+    'fastmri_knee_singlecoil_val_url',
+    None,
+    'Only necessary if you want this script to `wget` the FastMRI validation '
+    'split. If not, you can supply the path to --data_dir in '
+    'submission_runner.py.')
+
+flags.DEFINE_integer(
+    'num_decompression_threads',
+    8,
+    'The number of threads to use in parallel when decompressing.')
+FLAGS = flags.FLAGS
+
+
+def download_criteo(dataset_dir, tmp_dir, num_decompression_threads):
+  criteo_dir = os.path.join(dataset_dir, 'criteo')
+  tmp_criteo_dir = os.path.join(tmp_dir, 'criteo')
+  processes = []
+  for day in range(24):
+    if day in [0, 1, 2, 3, 23]:  # DO NOT SUBMIT
+      continue
+    logging.info(f'Downloading Criteo day {day}...')
+    #
+    # DOWNLOADING STRAIGHT TO gcsfuse mounted GCS IS 4000x SLOWER!!!!
+    #
+    wget_cmd = [
+        'wget',
+        f'--directory-prefix={tmp_criteo_dir}',
+        f'https://storage.googleapis.com/criteo-cail-datasets/day_{day}.gz'
+    ]
+    output_path = os.path.join(criteo_dir, f'day_{day}.csv')
+    unzip_cmd = [
+        'pigz',
+        '-d',
+        '-c',
+        f'-p{num_decompression_threads}',
+        os.path.join(tmp_criteo_dir, f'day_{day}.gz'),
+        f'> {output_path}'
+    ]
+    command_str = ' '.join([*wget_cmd, '&&', *unzip_cmd])
+    logging.info(f'Running Criteo download command:\n{command_str}')
+    # Note that shell=True can be dangerous if the user injects code into the
+    # --dataset_dir flag. We do some basic sanitization in main(), but
+    # submitters should not let untrusted users run this script on their
+    # systems.
+    processes.append(subprocess.Popen(command_str, shell=True))
+  for p in processes:
+    p.communicate()
+
+
+def download_fastmri(
+    dataset_dir,
+    tmp_dir,
+    knee_singlecoil_train_url,
+    knee_singlecoil_val_url):
+  pass
+
+
+def download_imagenet(
+    dataset_dir, tmp_dir, imagenet_train_url, imagenet_val_url):
+  pass
+
+
+def download_librispeech(dataset_dir, tmp_dir):
+  pass
+
+
+def download_ogbg(dataset_dir, tmp_dir):
+  pass
+
+
+def _download_wmt_helper(dataset_dir, filename):
+  wmt_dir = os.path.join(dataset_dir, 'wmt')
+  wget_args = ['-b', f'--directory-prefix={wmt_dir}']
+  if filename.startswith('wmt13'):
+    subdomain = 'www'
+  else:
+    subdomain = 'data'
+  subprocess.run(
+      'wget', f'https://{subdomain}.statmt.org/{filename}', *wget_args, check=True)
+
+
+def download_wmt(dataset_dir):
+  """WMT17 de-en, from https://www.statmt.org/wmt17/translation-task.html."""
+  _download_wmt_helper(dataset_dir, 'wmt13/training-parallel-europarl-v7.tgz')
+  _download_wmt_helper(dataset_dir, 'wmt13/training-parallel-commoncrawl.tgz')
+  _download_wmt_helper(dataset_dir,
+                       'wmt17/translation-task/training-parallel-nc-v12.tgz')
+  _download_wmt_helper(dataset_dir, 'wmt17/translation-task/rapid2016.tgz')
+
+
+def main(_):
+  dataset_dir = FLAGS.dataset_dir
+  tmp_dir = FLAGS.temp_dir
+  num_decompression_threads = FLAGS.num_decompression_threads
+  if ';' in dataset_dir or ' ' in dataset_dir or '&' in dataset_dir:
+    raise ValueError(f'Invalid dataset_dir: {dataset_dir}.')
+  dataset_dir = os.path.abspath(dataset_dir)
+  logging.info('Downloading data to %s...', dataset_dir)
+  if FLAGS.all or FLAGS.criteo:
+    logging.info('Downloading criteo...')
+    download_criteo(dataset_dir, tmp_dir, num_decompression_threads)
+  if FLAGS.all or FLAGS.fastmri:
+    logging.info('Downloading FastMRI...')
+    knee_singlecoil_train_url = FLAGS.fastmri_knee_singlecoil_train_url
+    knee_singlecoil_val_url = FLAGS.fastmri_knee_singlecoil_val_url
+    if knee_singlecoil_train_url is None or knee_singlecoil_val_url is None:
+      raise ValueError(
+          'Must provide both --fastmri_knee_singlecoil_{train,val}_url to '
+          'download the FastMRI dataset. Sign up for the URLs at '
+          'https://fastmri.med.nyu.edu/.')
+    # download_fastmri(
+    #     dataset_dir,
+    #     tmp_dir,
+    #     knee_singlecoil_train_url,
+    #     knee_singlecoil_val_url)
+  if FLAGS.all or FLAGS.fastmri:
+    logging.info('Downloading ImageNet...')
+    imagenet_train_url = FLAGS.imagenet_train_url
+    imagenet_val_url = FLAGS.imagenet_val_url
+    if imagenet_train_url is None or imagenet_val_url is None:
+      raise ValueError(
+          'Must provide both --imagenet_{train,val}_url to download the '
+          'ImageNet dataset. Sign up for the URLs at https://image-net.org/.')
+    # download_imagenet(
+    #     dataset_dir, tmp_dir, imagenet_train_url, imagenet_val_url)
+  if FLAGS.all or FLAGS.librispeech:
+    logging.info('Downloading Librispeech...')
+    # download_librispeech(dataset_dir, tmp_dir)
+  if FLAGS.all or FLAGS.ogbg:
+    logging.info('Downloading OGBG...')
+    # download_ogbg(dataset_dir, tmp_dir)
+  if FLAGS.all or FLAGS.wmt:
+    logging.info('Downloading WMT...')
+    # download_wmt(dataset_dir)
+
+
+if __name__ == '__main__':
+  app.run(main)
