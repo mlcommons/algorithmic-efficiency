b58dd5629d6a780971d6f8104b36ecb87acf9283
priyakasimbeg <priyakasimbeg@users.noreply.github.com>
2022-10-12 10:42:40 -0700

    Update input_pipeline.py

--- algorithmic_efficiency/workloads/fastmri/input_pipeline.py
+++ algorithmic_efficiency/workloads/fastmri/input_pipeline.py
@@ -178,12 +178,12 @@ def load_fastmri_split(global_batch_size,
   if split not in ['train', 'eval_train', 'validation', 'test']:
     raise ValueError('Unrecognized split {}'.format(split))
 
-  # Check if data directories exist because glob wil not raise an error
+  # Check if data directories exist because glob will not raise an error
   if not os.path.exists(os.path.join(data_dir, _TRAIN_DIR)):
-    raise NotADirectoryError("Directory not found: {}".format(
+    raise NotADirectoryError('Directory not found: {}'.format(
       os.path.join(data_dir, _TRAIN_DIR)))
   if not os.path.exists(os.path.join(data_dir, _VAL_DIR)):
-    raise NotADirectoryError("Directory not found: {}".format(
+    raise NotADirectoryError('Directory not found: {}'.format(
       os.path.join(data_dir, _VAL_DIR)))
 
   if split in ['train', 'eval_train']:
