c735b8a81a1751129f75798e85d206546e761a7e
pomonam <juhan.bae@mail.utoronto.ca>
2022-10-18 17:24:53 -0400

    Fix lint

--- algorithmic_efficiency/workloads/imagenet_resnet/imagenet_jax/models.py
+++ algorithmic_efficiency/workloads/imagenet_resnet/imagenet_jax/models.py
@@ -30,7 +30,7 @@ class ResNetBlock(nn.Module):
     y = self.conv(self.filters, (3, 3))(y)
     y = self.norm(scale_init=nn.initializers.zeros)(y)
 
-    if residual.shape != y.shape:
+    if residual.shape != y.shape or self.strides != (1, 1):
       residual = self.conv(
           self.filters, (1, 1), self.strides, name='Conv_proj')(
               residual)
@@ -59,7 +59,7 @@ class BottleneckResNetBlock(nn.Module):
     y = self.conv(self.filters * 4, (1, 1))(y)
     y = self.norm(scale_init=nn.initializers.zeros)(y)
 
-    if residual.shape != y.shape:
+    if residual.shape != y.shape or self.strides != (1, 1):
       residual = self.conv(
           self.filters * 4, (1, 1), self.strides, name='Conv_proj')(
               residual)
