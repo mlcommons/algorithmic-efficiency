fixing eval masking in criteo pytorch

algorithmic_efficiency/workloads/criteo1tb/criteo1tb_pytorch/metrics.py
@@ -3,9 +3,12 @@
 import torch
 
 
-def per_example_sigmoid_binary_cross_entropy(logits, targets):
+def per_example_sigmoid_binary_cross_entropy(logits, targets, mask_batch):
   ls = torch.nn.LogSigmoid()
   log_p = ls(logits)
   log_not_p = ls(-logits)
   per_example_losses = -1.0 * (targets * log_p + (1 - targets) * log_not_p)
+  if mask_batch is not None:
+    mask_batch = torch.reshape(mask_batch, (mask_batch.shape[0],))
+    per_example_losses *= mask_batch
   return per_example_losses

algorithmic_efficiency/workloads/criteo1tb/criteo1tb_pytorch/workload.py
@@ -32,12 +32,7 @@ class Criteo1TbDlrmSmallWorkload(BaseCriteo1TbDlrmSmallWorkload):
     label_batch = torch.reshape(label_batch, (batch_size,))
     logits_batch = torch.reshape(logits_batch, (batch_size,))
     per_example_losses = metrics.per_example_sigmoid_binary_cross_entropy(
-        logits=logits_batch, targets=label_batch)
-    # This should be unnecessary, but just to be safe.
-    per_example_losses = torch.reshape(per_example_losses, (batch_size,))
-    if mask_batch is not None:
-      mask_batch = torch.reshape(mask_batch, (batch_size,))
-      per_example_losses *= mask_batch
+        logits=logits_batch, targets=label_batch, mask_batch=mask_batch)
     return per_example_losses
 
   def _eval_metric(self, logits: spec.Tensor,
@@ -188,8 +183,10 @@ class Criteo1TbDlrmSmallWorkload(BaseCriteo1TbDlrmSmallWorkload):
         dropout_rate=None,
         aux_dropout_rate=None,
         update_batch_norm=False)
-    per_example_losses = metrics.per_example_sigmoid_binary_cross_entropy(
-        logits, batch['targets'])
+    per_example_losses = self.loss_fn(
+        label_batch=batch['targets'],
+        logits_batch=logits,
+        mask_batch=batch['weights'])
     batch_loss_numerator = torch.sum(per_example_losses).cpu().numpy()
     batch_loss_denominator = torch.sum(batch['weights']).cpu().numpy()
     return batch_loss_numerator, batch_loss_denominator