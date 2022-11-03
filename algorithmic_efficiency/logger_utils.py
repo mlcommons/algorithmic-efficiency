728d83bc22b607a584046ac667f1f7d27fc44243
Priya Kasimbeg <kasimbeg@google.com>
2022-11-03 05:34:39 +0000

    small change to logging error message.

--- algorithmic_efficiency/logger_utils.py
+++ algorithmic_efficiency/logger_utils.py
@@ -75,6 +75,7 @@ def get_log_dir(experiment_dir,
           run_dir = f'run_{_get_last_run_dir_index(runs) + 1}'
       else:
         raise ValueError(
+            'The flag --interactive=False. '
             'Please use --resume_from_last_run flag if --interactive=False.')
     else:
       run_dir = 'run_0'
