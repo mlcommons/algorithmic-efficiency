77608d21162e57e9f13376fefb0cb7d2b13f6f69
sourabh2k15 <smedapati@google.com>
2023-01-23 12:22:04 -0800

    Update check_gpu.py

--- check_gpu.py
+++ check_gpu.py
@@ -3,3 +3,6 @@ import jax
 print('Checking GPU presence in JAX')
 print(jax.local_device_count())
 print(jax.device_count())
+
+rng = jax.random.PRNGKey(0)
+data_rng, shuffle_rng = jax.random.split(2)
