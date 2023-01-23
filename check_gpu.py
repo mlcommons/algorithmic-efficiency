8e18c4bc1447b94571fd28f6f05fa7773a3ea18d
sourabh2k15 <smedapati@google.com>
2023-01-23 12:23:35 -0800

    Update check_gpu.py

--- check_gpu.py
+++ check_gpu.py
@@ -5,4 +5,4 @@ print(jax.local_device_count())
 print(jax.device_count())
 
 rng = jax.random.PRNGKey(0)
-data_rng, shuffle_rng = jax.random.split(2)
+data_rng, shuffle_rng = jax.random.split(rng, 2)
