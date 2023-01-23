f5a901f6b5cff7def5de6649860b5a0087386a2e
sourabh2k15 <smedapati@google.com>
2023-01-23 12:20:48 -0800

    Update startup.sh

--- docker-scripts/startup.sh
+++ docker-scripts/startup.sh
@@ -3,16 +3,16 @@
 echo "This trick works!!"
 
 python3 check_gpu.py
-echo "Running OGBG"
+# echo "Running OGBG"
 
-# cd ..
-# ls ./google-cloud-sdk/bin/gsutil
+# # cd ..
+# # ls ./google-cloud-sdk/bin/gsutil
 
-# echo $PATH
+# # echo $PATH
 
-# cd algorithmic-efficiency
+# # cd algorithmic-efficiency
 
-yes | python3 submission_runner.py     --framework=jax     --workload=ogbg --submission_path=reference_algorithms/target_setting_algorithms/jax_nesterov.py     --tuning_search_space=reference_algorithms/target_setting_algorithms/ogbg/tuning_search_space.json --data_dir=../data/ --num_tuning_trials=1 --experiment_dir=../experiment_runs/  --max_global_steps=1 --experiment_name=docker_ogbg
+# yes | python3 submission_runner.py     --framework=jax     --workload=ogbg --submission_path=reference_algorithms/target_setting_algorithms/jax_nesterov.py     --tuning_search_space=reference_algorithms/target_setting_algorithms/ogbg/tuning_search_space.json --data_dir=../data/ --num_tuning_trials=1 --experiment_dir=../experiment_runs/  --max_global_steps=1 --experiment_name=docker_ogbg
 
 # cd ..
 # ./google-cloud-sdk/bin/gsutil -m cp -r experiment_runs/* gs://mlcommons-data/experiment_runs/docker_ogbg 
