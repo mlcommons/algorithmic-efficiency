8eb76d334ac12373b0ff3b72df00f9e02cba857e
Sourabh <smedapati@google.com>
2023-01-24 23:25:43 +0000

    force adding startup script

---
+++ docker/scripts/startup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Checking GPU presence and CUDA linking"
+
+python3 docker/scripts/check_gpu.py
+echo "Running OGBG"
+
+
+yes | python3 submission_runner.py     --framework=jax     --workload=ogbg --submission_path=reference_algorithms/target_setting_algorithms/jax_nesterov.py     --tuning_search_space=reference_algorithms/target_setting_algorithms/ogbg/tuning_search_space.json --data_dir=../data/ --num_tuning_trials=1 --experiment_dir=../experiment_runs/  --experiment_name=docker_ogbg
+
+cd ..
+./google-cloud-sdk/bin/gsutil -m cp -r experiment_runs/* gs://mlcommons-data/experiment_runs/
