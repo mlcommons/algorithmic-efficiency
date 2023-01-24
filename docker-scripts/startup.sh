edc07850cff191389ac50b49de05359aeb4e36f7
sourabh2k15 <smedapati@google.com>
2023-01-24 14:16:29 -0800

    Update startup.sh

--- docker-scripts/startup.sh
+++ docker-scripts/startup.sh
@@ -14,5 +14,5 @@ echo "Running OGBG"
 
 yes | python3 submission_runner.py     --framework=jax     --workload=ogbg --submission_path=reference_algorithms/target_setting_algorithms/jax_nesterov.py     --tuning_search_space=reference_algorithms/target_setting_algorithms/ogbg/tuning_search_space.json --data_dir=../data/ --num_tuning_trials=1 --experiment_dir=../experiment_runs/  --max_global_steps=1 --experiment_name=docker_ogbg
 
-# cd ..
-# ./google-cloud-sdk/bin/gsutil -m cp -r experiment_runs/* gs://mlcommons-data/experiment_runs/docker_ogbg 
+cd ..
+./google-cloud-sdk/bin/gsutil -m cp -r experiment_runs/* gs://mlcommons-data/experiment_runs/
