44f4a2441304f93c0f07123963ea8d8248972e76
sourabh2k15 <smedapati@google.com>
2023-01-26 22:00:54 -0800

    Update startup.sh

--- docker/scripts/startup.sh
+++ docker/scripts/startup.sh
@@ -12,7 +12,7 @@ do
 done
 
 cd ..
-./google-cloud-sdk/bin/gsutil -m cp gs://mlcommons-data/criteo/* data/
+./google-cloud-sdk/bin/gsutil -m cp -r gs://mlcommons-data/criteo/* data/
 # yes | python3 dataset_setup.py --data_dir=~/data --temp_dir=~/data --all=False --criteo
 
 cd algorithmic-efficiency
@@ -21,7 +21,7 @@ echo "Checking GPU presence and CUDA linking"
 python3 docker/scripts/check_gpu.py
 echo "Running Experiment"
 
-python3 submission_runner.py     --framework=$FRAMEWORK     --workload=$WORKLOAD --submission_path=$SUBMISSION_PATH     --tuning_search_space=$TUNING_SEARCH_SPACE --data_dir=../ --num_tuning_trials=1 --experiment_dir=../experiment_runs/  --experiment_name=$EXPERIMENT_NAME
+python3 submission_runner.py     --framework=$FRAMEWORK     --workload=$WORKLOAD --submission_path=$SUBMISSION_PATH     --tuning_search_space=$TUNING_SEARCH_SPACE --data_dir=../data --num_tuning_trials=1 --experiment_dir=../experiment_runs/  --experiment_name=$EXPERIMENT_NAME
 
 cd ..
 ./google-cloud-sdk/bin/gsutil -m cp -r experiment_runs/* gs://mlcommons-data/experiment_runs/
