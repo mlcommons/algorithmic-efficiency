ff364c175342b02f16adefd12cb59c31629222dd
Sourabh Medapati <smedapati@google.com>
2023-01-27 02:39:44 +0000

    criteo docker image

--- docker/scripts/startup.sh
+++ docker/scripts/startup.sh
@@ -11,14 +11,14 @@ do
     esac
 done
 
-ls data
-# echo "Checking GPU presence and CUDA linking"
+ls 
+echo "Checking GPU presence and CUDA linking"
 
-# python3 docker/scripts/check_gpu.py
-# echo "Running Experiment"
+python3 docker/scripts/check_gpu.py
+echo "Running Experiment"
 
 
-# python3 submission_runner.py     --framework=$FRAMEWORK     --workload=$WORKLOAD --submission_path=$SUBMISSION_PATH     --tuning_search_space=$TUNING_SEARCH_SPACE --data_dir=../data/ --num_tuning_trials=1 --experiment_dir=../experiment_runs/  --experiment_name=$EXPERIMENT_NAME
+python3 submission_runner.py     --framework=$FRAMEWORK     --workload=$WORKLOAD --submission_path=$SUBMISSION_PATH     --tuning_search_space=$TUNING_SEARCH_SPACE --data_dir=../data/ --num_tuning_trials=1 --experiment_dir=../experiment_runs/  --experiment_name=$EXPERIMENT_NAME
 
-# cd ..
-# ./google-cloud-sdk/bin/gsutil -m cp -r experiment_runs/* gs://mlcommons-data/experiment_runs/
+cd ..
+./google-cloud-sdk/bin/gsutil -m cp -r experiment_runs/* gs://mlcommons-data/experiment_runs/
