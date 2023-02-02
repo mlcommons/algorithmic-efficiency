Fix normalization

algorithmic_efficiency/workloads/cifar/cifar_jax/input_pipeline.py
@@ -54,8 +54,7 @@ def preprocess_for_train(image: spec.Tensor,
   image = tf.image.stateless_random_crop(
       image, (crop_size, crop_size, 3), seed=crop_rng)
   image = tf.image.stateless_random_flip_left_right(image, seed=flip_rng)
-  image = normalize_image(image, mean_rgb, stddev_rgb, dtype=tf.float32)
-  image = tf.image.convert_image_dtype(image, dtype=dtype)
+  image = normalize_image(image, mean_rgb, stddev_rgb, dtype=dtype)
   return image
 
 

algorithmic_efficiency/workloads/imagenet_resnet/imagenet_jax/input_pipeline.py
@@ -154,8 +154,7 @@ def normalize_image(image: spec.Tensor,
                     mean_rgb: Tuple[float, float, float],
                     stddev_rgb: Tuple[float, float, float],
                     dtype=tf.float32) -> spec.Tensor:
-  if image.dtype == tf.uint8:
-    image = tf.image.convert_image_dtype(image, dtype)
+  image = tf.image.convert_image_dtype(image, dtype)
   image -= tf.constant(mean_rgb, shape=[1, 1, 3], dtype=image.dtype)
   image /= tf.constant(stddev_rgb, shape=[1, 1, 3], dtype=image.dtype)
   return image