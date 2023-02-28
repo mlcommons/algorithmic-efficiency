dde8253f0572d605c657cc24bcaf87955a6085d2
Priya Kasimbeg <kasimbeg@google.com>
2023-02-28 08:09:10 +0000

    default args for startup docker script

--- docker/scripts/startup.sh
+++ docker/scripts/startup.sh
@@ -1,5 +1,9 @@
 #!bin/bash
 
+# Defaults
+DEBUG_MODE="false"
+SUBMISSION_PATH="false"
+
 while getopts d:f:s:t:e:w:b: flag
 do
     case "${flag}" in
@@ -13,10 +17,11 @@ do
     esac
 done
 
-RUNS_BUCKET="gs://mlcommons-runs/"
-ROOT_DATA_BUCKET="gs://mlcommons-data/"
-ROOT_DATA_DIR="/data/"
-EXPERIMENT_DIR="/experiment_runs/"
+ROOT_DATA_BUCKET="gs://mlcommons-data"
+ROOT_DATA_DIR="/data"
+
+EXPERIMENT_BUCKET="gs://mlcommons-runs"
+EXPERIMENT_DIR="/experiment_runs"
 
 if [ "${DATASET}" == "imagenet" ]
 then 
@@ -30,30 +35,32 @@ fi
 # Copy data from MLCommons bucket if data does not downloaded yet
 if [ ! -d ${DATA_DIR} ]
 then
+    mkdir -p ${DATA_DIR}
     ./google-cloud-sdk/bin/gsutil -m cp -r ${DATA_BUCKET}/* ${DATA_DIR}
 fi 
 
 # Check GPU requirements and run experiment
-python3 docker/scripts/check_gpu.py
+# python3 docker/scripts/check_gpu.py
 
 # Optionally run workload
 if $SUBMISSION_PATH
 then
-python3 algorithmic-efficiency/submission_runner.py \
+cd algorithmic-efficiency
+python3 submission_runner.py \
     --framework=${FRAMEWORK}  \
     --workload=${WORKLOAD} \
     --submission_path=${SUBMISSION_PATH}  \
     --tuning_search_space=${TUNING_SEARCH_SPACE}  \
-    --data_dir=${DATA_DIR} 
+    --data_dir=${DATA_DIR} \
     --num_tuning_trials=1  \
     --experiment_dir=${EXPERIMENT_DIR}  \
     --experiment_name=${EXPERIMENT_NAME}  \
 
-gsutil -m cp -r ${EXPERIMENT_DIR}/${EXPERIMENT_NAME}/* ${RUNS_BUCKET}
+./google-cloud-sdk/bin/ gsutil -m cp -r ${EXPERIMENT_DIR}/${EXPERIMENT_NAME}/* gs://${EXPERIMENT_BUCKET}/${EXPERIMENT_NAME}
 fi
 
 # Keep main process running in debug mode to avoid the container from stopping
-if [${DEBUG_MODE} == 'true']
+if [ ${DEBUG_MODE} == 'true' ]
 then 
     while true
     do 
