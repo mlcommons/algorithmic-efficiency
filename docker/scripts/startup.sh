92acf37eff4533f68b7c860994eadf889a7dff30
Priya Kasimbeg <kasimbeg@google.com>
2023-03-01 19:26:11 +0000

    add torch run prefix and fix if condition

--- docker/scripts/startup.sh
+++ docker/scripts/startup.sh
@@ -2,7 +2,6 @@
 
 # Defaults
 DEBUG_MODE="false"
-SUBMISSION_PATH="false"
 
 while getopts d:f:s:t:e:w:b: flag
 do
@@ -22,6 +21,11 @@ ROOT_DATA_DIR="/data"
 
 EXPERIMENT_BUCKET="gs://mlcommons-runs"
 EXPERIMENT_DIR="/experiment_runs"
+
+if [ "${FRAMEWORK}" == "pytorch" ]
+then 
+    TORCH_RUN_COMMAND_PREFIX="torchrun --redirects 1:0,2:0,3:0,4:0,5:0,6:0,7:0 --standalone --nnodes=1 --nproc_per_node=8"
+fi
 
 if [ "${DATASET}" == "imagenet" ]
 then 
@@ -43,24 +47,25 @@ fi
 # python3 scripts/check_gpu.py
 
 # Optionally run workload
-if ${SUBMISSION_PATH}
-then
-LOG_DIR="logs/${EXPERIMENT_NAME}"
-LOG_FILE="$LOG_DIR/submission.log"
-mkdir -p ${LOG_DIR}
-cd algorithmic-efficiency
-python3 submission_runner.py \
-    --framework=${FRAMEWORK}  \
-    --workload=${WORKLOAD} \
-    --submission_path=${SUBMISSION_PATH}  \
-    --tuning_search_space=${TUNING_SEARCH_SPACE}  \
-    --data_dir=${DATA_DIR} \
-    --num_tuning_trials=1  \
-    --experiment_dir=${EXPERIMENT_DIR}  \
-    --experiment_name=${EXPERIMENT_NAME}  2>&1 | tee ${LOG_FILE}
+if [ ! -z ${SUBMISSION_PATH+x} ]
+    then
+    LOG_DIR="/logs"
+    LOG_FILE="$LOG_DIR/${WORKLOAD}_${FRAMEWORK}_submission.log"
+    mkdir -p ${LOG_DIR}
+    cd algorithmic-efficiency
+    # The TORCH_RUN_COMMAND_PREFIX is only set if FRAMEWORK is "pytorch"
+    ${TORCH_RUN_COMMAND_PREFIX} python3 submission_runner.py \
+        --framework=${FRAMEWORK}  \
+        --workload=${WORKLOAD} \
+        --submission_path=${SUBMISSION_PATH}  \
+        --tuning_search_space=${TUNING_SEARCH_SPACE}  \
+        --data_dir=${DATA_DIR} \
+        --num_tuning_trials=1  \
+        --experiment_dir=${EXPERIMENT_DIR}  \
+        --experiment_name=${EXPERIMENT_NAME}  2>&1 | tee ${LOG_FILE}
 
-./google-cloud-sdk/bin/ gsutil -m cp -r ${EXPERIMENT_DIR}/${EXPERIMENT_NAME}/* gs://${EXPERIMENT_BUCKET}/${EXPERIMENT_NAME}
-./google-cloud-sdk/bin/ gsutil -m cp -r ${LOG_FILE} gs://${EXPERIMENT_BUCKET}/${EXPERIMENT_NAME}
+    ./google-cloud-sdk/bin/ gsutil -m cp -r ${EXPERIMENT_DIR}/${EXPERIMENT_NAME}/* gs://${EXPERIMENT_BUCKET}/${EXPERIMENT_NAME}
+    ./google-cloud-sdk/bin/ gsutil -m cp -r ${LOG_FILE} gs://${EXPERIMENT_BUCKET}/${EXPERIMENT_NAME}
 
 fi
 
