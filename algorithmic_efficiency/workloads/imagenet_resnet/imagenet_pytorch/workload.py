ff82c43f30285f5f44cdcaef825740670f7954e0
Priya Kasimbeg <kasimbeg@google.com>
2023-03-08 23:06:02 +0000

    fix syntax errors

--- algorithmic_efficiency/workloads/imagenet_resnet/imagenet_pytorch/workload.py
+++ algorithmic_efficiency/workloads/imagenet_resnet/imagenet_pytorch/workload.py
@@ -237,7 +237,7 @@ class ImagenetResNetWorkload(BaseImagenetResNetWorkload):
     summed_loss = per_example_losses.sum()
     return {
         'summed': summed_loss,
-        'n_valid_examples': torch.tensor(n_valid_examples, device=DEVICE)
+        'n_valid_examples': torch.tensor(n_valid_examples, device=DEVICE),
         'per_example': per_example_losses,
     }
 
