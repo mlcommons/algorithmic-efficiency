34cbda2e715a15167492e0a667a5ef7d0f944972
priyakasimbeg <priyakasimbeg@users.noreply.github.com>
2023-03-20 12:25:17 -0700

    Update workload.py

--- algorithmic_efficiency/workloads/criteo1tb/criteo1tb_pytorch/workload.py
+++ algorithmic_efficiency/workloads/criteo1tb/criteo1tb_pytorch/workload.py
@@ -160,7 +160,9 @@ class Criteo1TbDlrmSmallWorkload(BaseCriteo1TbDlrmSmallWorkload):
         if not_train:
           weights = batch.get('weights')
           if weights is not None:
-            weights = torch.as_tensor(weights, dtype=torch.float32, device=DEVICE)               
+            weights = torch.as_tensor(weights, 
+                                      dtype=torch.float32, 
+                                      device=DEVICE)               
 
         # Send batch to other devices when using DDP.
         if USE_PYTORCH_DDP:
