c29ccd8085cbd76befe5792a43c6c0222a36430e
Priya Kasimbeg <kasimbeg@google.com>
2023-03-21 01:42:25 +0000

    add debugging breakpoint

--- algorithmic_efficiency/workloads/criteo1tb/criteo1tb_pytorch/workload.py
+++ algorithmic_efficiency/workloads/criteo1tb/criteo1tb_pytorch/workload.py
@@ -170,6 +170,7 @@ class Criteo1TbDlrmSmallWorkload(BaseCriteo1TbDlrmSmallWorkload):
             per_device_batch_size = torch.tensor(
                 len(targets[0]), dtype=torch.int32, device=DEVICE)
             dist.broadcast(per_device_batch_size, src=0)
+            breakpoint()
             dist.broadcast(weights, src=0)
             weights = weights[0]
           dist.broadcast(inputs, src=0)
