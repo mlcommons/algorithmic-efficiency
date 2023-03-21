d10ee77405ec3a26f8c1b852fd8349c4ae885a31
priyakasimbeg <priyakasimbeg@users.noreply.github.com>
2023-03-21 10:42:21 -0700

    Update formatting in criteo workload.py

--- algorithmic_efficiency/workloads/criteo1tb/criteo1tb_pytorch/workload.py
+++ algorithmic_efficiency/workloads/criteo1tb/criteo1tb_pytorch/workload.py
@@ -160,9 +160,12 @@ class Criteo1TbDlrmSmallWorkload(BaseCriteo1TbDlrmSmallWorkload):
         if not_train:
           weights = batch.get('weights')
           if weights is None:
-            weights = torch.ones((N_GPUS, per_device_batch_size, 1), dtype=torch.float32, device=DEVICE)
+            weights = torch.ones((N_GPUS, per_device_batch_size, 1), 
+                                  dtype=torch.float32,
+                                  device=DEVICE)
           else:
-            weights = torch.as_tensor(weights, dtype=torch.float32, device=DEVICE)
+            weights = torch.as_tensor(
+                weights, dtype=torch.float32, device=DEVICE)
         # Send batch to other devices when using DDP.
         if USE_PYTORCH_DDP:
           if not_train:
