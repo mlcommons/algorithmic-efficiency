874fdc70e2094fc9773b746f773b21d81eeeaec7
Priya Kasimbeg <kasimbeg@google.com>
2023-05-01 20:37:46 +0000

    formatting

--- scoring/scoring_utils.py
+++ scoring/scoring_utils.py
@@ -7,30 +7,30 @@ metrics_prefix = 'Metrics: '
 
 #### File IO helper functions ###
 def get_logfile_paths(logdir):
-    """Gets all files ending in .log in logdir
+  """Gets all files ending in .log in logdir
     """
-    filenames = os.listdir(logdir)
-    logfile_paths= []
-    for f in filenames:
-        if f.endswith(".log"):
-            f = os.path.join(logdir, f)
-            logfile_paths.append(f)
-    return logfile_paths
+  filenames = os.listdir(logdir)
+  logfile_paths = []
+  for f in filenames:
+    if f.endswith(".log"):
+      f = os.path.join(logdir, f)
+      logfile_paths.append(f)
+  return logfile_paths
 
 
 ### Logfile reading helper functions ###
 def get_metrics_line(logfile):
-    """Extracts the metrics line from a logfile.
+  """Extracts the metrics line from a logfile.
     """
-    with open(logfile, 'r') as f:
-        for line in f:
-            if metrics_prefix in line:
-                return line
-    raise ValueError(f"Log file does not have a metrics line {logfile}")
+  with open(logfile, 'r') as f:
+    for line in f:
+      if metrics_prefix in line:
+        return line
+  raise ValueError(f"Log file does not have a metrics line {logfile}")
 
 
 def convert_metrics_line_to_dict(line):
-    """Convert metrics line to dict.
+  """Convert metrics line to dict.
     Args:
         line: str 
 
@@ -40,40 +40,40 @@ def convert_metrics_line_to_dict(line):
             e.g. {'loss':[5.1, 3.2, 1.0],
                   'step':[100, 200, 300]}
     """
-    eval_results = []
-    dict_str = line.split(metrics_prefix)[1]
-    dict_str = dict_str.replace("'", "\"")
-    dict_str = dict_str.replace("(", "")
-    dict_str = dict_str.replace(")", "")
-    dict_str = dict_str.replace("DeviceArray", "")
-    dict_str = dict_str.replace(", dtype=float32", "")
-    dict_str = dict_str.replace("nan", "0")
-    try:
-        metrics_dict = json.loads(dict_str)
-    except Exception as e:
-        print('Error loading metrics line')
-        raise(e)
-    for item in metrics_dict['eval_results']:
-        if isinstance(item, dict):
-            eval_results.append(item)
-    
-    keys = eval_results[0].keys()
+  eval_results = []
+  dict_str = line.split(metrics_prefix)[1]
+  dict_str = dict_str.replace("'", "\"")
+  dict_str = dict_str.replace("(", "")
+  dict_str = dict_str.replace(")", "")
+  dict_str = dict_str.replace("DeviceArray", "")
+  dict_str = dict_str.replace(", dtype=float32", "")
+  dict_str = dict_str.replace("nan", "0")
+  try:
+    metrics_dict = json.loads(dict_str)
+  except Exception as e:
+    print('Error loading metrics line')
+    raise (e)
+  for item in metrics_dict['eval_results']:
+    if isinstance(item, dict):
+      eval_results.append(item)
 
-    dict_of_lists = {}
-    for key in keys:
-        dict_of_lists[key] = []
-   
-    for eval_results_dict in eval_results:
-        for key in eval_results_dict.keys():
-            val = eval_results_dict[key]
-            dict_of_lists[key].append(val)
-    
-    return dict_of_lists
+  keys = eval_results[0].keys()
+
+  dict_of_lists = {}
+  for key in keys:
+    dict_of_lists[key] = []
+
+  for eval_results_dict in eval_results:
+    for key in eval_results_dict.keys():
+      val = eval_results_dict[key]
+      dict_of_lists[key].append(val)
+
+  return dict_of_lists
 
 
-### Results formatting helper functions ### 
+### Results formatting helper functions ###
 def get_results_dict(logfile):
-    """
+  """
     Returns results as dict.
 
     Returns:
@@ -82,13 +82,13 @@ def get_results_dict(logfile):
         {'loss':[5.1, 3.2, 1.0],
          'step':[100, 200, 300]}
     """
-    results_line = get_metrics_line(logfile)
-    results_dict = convert_metrics_line_to_dict(results_line)
-    return results_dict
+  results_line = get_metrics_line(logfile)
+  results_dict = convert_metrics_line_to_dict(results_line)
+  return results_dict
 
 
 def get_tuning_run_df(logfile):
-    """Get dataframe for single tuning run from logfile.
+  """Get dataframe for single tuning run from logfile.
     Args:
         logfile: str path to logfile.
 
@@ -96,13 +96,13 @@ def get_tuning_run_df(logfile):
         DataFrame where indices are index of eval and 
         columns are metric names.
     """
-    line = get_metrics_line(logfile)
-    tuning_run_dict = convert_metrics_line_to_dict(line)
-    return pd.DataFrame(tuning_run_dict)
+  line = get_metrics_line(logfile)
+  tuning_run_dict = convert_metrics_line_to_dict(line)
+  return pd.DataFrame(tuning_run_dict)
 
 
 def get_trials_df(experiment_dir):
-    """Gets a df of per trial results from an experiment_dir.
+  """Gets a df of per trial results from an experiment_dir.
     This df can be provided as input to 
     scoring.compute_performance_profiles. 
     Args:
@@ -126,21 +126,15 @@ def get_trials_df(experiment_dir):
             | trial_1 | [5.1, 3.2, 1.0] | [100, 200, 300] |
             +---------+-----------------+-----------------+
     """
-    trial_dirs = os.listdir(experiment_dir)
-    trials_dict = {}
-    for trial_dir in trial_dirs:
-        logfile_paths = get_logfile_paths(os.path.join(experiment_dir, trial_dir))
-        if len(logfile_paths) > 1:
-            logfile_path=logfile_paths[-1]
-        else:
-            logfile_path=logfile_paths[0]
-        results_dict = get_results_dict(logfile_path)
-        trials_dict[trial_dir] = results_dict
-    df =  pd.DataFrame(trials_dict).transpose()
-    return df
-
-
-
-
-
-
+  trial_dirs = os.listdir(experiment_dir)
+  trials_dict = {}
+  for trial_dir in trial_dirs:
+    logfile_paths = get_logfile_paths(os.path.join(experiment_dir, trial_dir))
+    if len(logfile_paths) > 1:
+      logfile_path = logfile_paths[-1]
+    else:
+      logfile_path = logfile_paths[0]
+    results_dict = get_results_dict(logfile_path)
+    trials_dict[trial_dir] = results_dict
+  df = pd.DataFrame(trials_dict).transpose()
+  return df
--- scoring/test_scoring_utils.py
+++ scoring/test_scoring_utils.py
@@ -1,20 +1,22 @@
 from absl.testing import absltest
-import scoring_utils 
+import scoring_utils
 
 TEST_LOGFILE = 'test_data/trial_0/adamw_fastmri_jax_04-18-2023-13-10-58.log'
 EXPERIMENT_DIR = 'test_data/'
 
+
 class Test(absltest.TestCase):
 
-    def test_convert_metrics_line_to_dict(self):
-        line = scoring_utils.get_metrics_line(TEST_LOGFILE)
-        results_dict = scoring_utils.convert_metrics_line_to_dict(line)
+  def test_convert_metrics_line_to_dict(self):
+    line = scoring_utils.get_metrics_line(TEST_LOGFILE)
+    results_dict = scoring_utils.convert_metrics_line_to_dict(line)
 
-    def test_get_tuning_run_df(self):
-        df = scoring_utils.get_tuning_run_df(TEST_LOGFILE)
+  def test_get_tuning_run_df(self):
+    df = scoring_utils.get_tuning_run_df(TEST_LOGFILE)
 
-    def test_get_trials_df(self):
-        df = scoring_utils.get_trials_df(EXPERIMENT_DIR)
+  def test_get_trials_df(self):
+    df = scoring_utils.get_trials_df(EXPERIMENT_DIR)
+
 
 if __name__ == '__main__':
-    absltest.main()
+  absltest.main()
