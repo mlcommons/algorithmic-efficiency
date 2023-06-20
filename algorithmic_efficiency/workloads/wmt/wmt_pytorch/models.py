ce68a39d87b3c0d6837fff1c43d5d22e32263d10
pomonam <juhan.bae@mail.utoronto.ca>
2023-06-20 01:55:31 -0400

    Linting fix

--- algorithmic_efficiency/workloads/wmt/wmt_pytorch/models.py
+++ algorithmic_efficiency/workloads/wmt/wmt_pytorch/models.py
@@ -217,10 +217,10 @@ class TransformerEncoder(nn.Module):
         rate is high.
 
   Examples::
-      >>> encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
-      >>> transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=6)
-      >>> src = torch.rand(10, 32, 512)
-      >>> out = transformer_encoder(src)
+    >>> encoder_layer = nn.TransformerEncoderLayer(12, 8)
+    >>> transformer_encoder = nn.TransformerEncoder(encoder_layer, 6)
+    >>> src = torch.rand(10, 32, 512)
+    >>> out = transformer_encoder(src)
   """
   __constants__ = ['norm']
 
@@ -283,7 +283,8 @@ class TransformerEncoder(nn.Module):
         f' {str_first_layer}.activation_relu_or_gelu was not True'
     elif not first_layer.norm1.eps == first_layer.norm2.eps:
       why_not_sparsity_fast_path = \
-        f'{str_first_layer}.norm1.eps was not equal to {str_first_layer}.norm2.eps'
+        f'{str_first_layer}.norm1.eps was not equal ' \
+        f'to {str_first_layer}.norm2.eps'
     elif not src.dim() == 3:
       why_not_sparsity_fast_path = \
         f'input not batched; expected src.dim() of 3 but got {src.dim()}'
@@ -618,10 +619,11 @@ class TransformerEncoderLayer(nn.Module):
     elif not self.self_attn.batch_first:
       why_not_sparsity_fast_path = '`self_attn.batch_first` was not True'
     elif not self.self_attn._qkv_same_embed_dim:
-      why_not_sparsity_fast_path = '`self_attn._qkv_same_embed_dim` was not True'
+      why_not_sparsity_fast_path = \
+        '`self_attn._qkv_same_embed_dim` was not True'
     elif not self.activation_relu_or_gelu:
       why_not_sparsity_fast_path = '`activation_relu_or_gelu` was not True'
-    elif not (self.norm1.eps == self.norm2.eps):
+    elif not self.norm1.eps == self.norm2.eps:
       why_not_sparsity_fast_path = '`norm1.eps` is not equal to norm2.eps'
     elif src_mask is not None:
       why_not_sparsity_fast_path = '`src_mask` is not supported for fastpath'
@@ -650,12 +652,13 @@ class TransformerEncoderLayer(nn.Module):
           self.linear2.bias,
       )
 
-      # We have to use list comprehensions below because TorchScript does not support
-      # generator expressions.
+      # We have to use list comprehensions below because TorchScript
+      # does not support generator expressions.
       if torch.overrides.has_torch_function(tensor_args):
         why_not_sparsity_fast_path = 'Some Tensor argument has_torch_function'
       elif not all((x.is_cuda or 'cpu' in str(x.device)) for x in tensor_args):
-        why_not_sparsity_fast_path = 'Some Tensor argument is neither CUDA nor CPU'
+        why_not_sparsity_fast_path = \
+          'Some Tensor argument is neither CUDA nor CPU'
       elif torch.is_grad_enabled() and any(
           x.requires_grad for x in tensor_args):
         why_not_sparsity_fast_path = (
@@ -682,7 +685,6 @@ class TransformerEncoderLayer(nn.Module):
             self.linear1.bias,
             self.linear2.weight,
             self.linear2.bias,
-            # TODO: if src_mask and src_key_padding_mask merge to single 4-dim mask
             src_mask if src_mask is not None else src_key_padding_mask,
             1 if src_key_padding_mask is not None else
             0 if src_mask is not None else None,
@@ -722,18 +724,18 @@ class TransformerEncoderLayer(nn.Module):
 class TransformerDecoder(nn.Module):
   r"""TransformerDecoder is a stack of N decoder layers
   Args:
-    d_model: the number of expected features in the input (default=1024).
-    nhead: the number of heads in the multiheadattention models (default=16).
+    d_model: the number of expected features in the input (default=1024)
+    nhead: the number of heads in the multiheadattention models (default=16)
     d_hid: the dimension of the feedforward network model
-        (default=1024).
-    dropout_rate: the dropout_rate value (default=0.1).
+        (default=1024)
+    dropout_rate: the dropout_rate value (default=0.1)
     layer_norm_eps: the eps value in layer normalization components
         (default=1e-6).
-    decoder_layer: an instance of the TransformerDecoderLayer() class (required)
-    num_layers: the number of sub-decoder-layers in the decoder (required).
+    decoder_layer: an instance of the TransformerDecoderLayer() class
+    num_layers: the number of sub-decoder-layers in the decoder
   Examples::
-    >>> decoder_layer = nn.TransformerDecoderLayer(d_model=512, nhead=8)
-    >>> transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=6)
+    >>> decoder_layer = nn.TransformerDecoderLayer(12, 8)
+    >>> transformer_decoder = nn.TransformerDecoder(decoder_layer, 6)
     >>> memory = torch.rand(10, 32, 512)
     >>> tgt = torch.rand(20, 32, 512)
     >>> out = transformer_decoder(tgt, memory)
