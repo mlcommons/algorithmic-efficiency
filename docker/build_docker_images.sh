4d822280788b237468705ea98fa2c8befa3ae21f
Priya Kasimbeg <kasimbeg@google.com>
2023-06-26 23:48:43 +0000

    add regression tests

---
+++ docker/build_docker_images.sh
@@ -0,0 +1,19 @@
+for FRAMEWORK in "jax" "pytorch" "joint"
+do
+    GIT_BRANCH="dev"
+    IMAGE_NAME="algoperf_$FRAMEWORK:$GIT_BRANCH"
+    DOCKER_BUILD_COMMAND="docker build -t $IMAGE_NAME . --build-arg framework=$FRAMEWORK --build-arg branch=$GIT_BRANCH"
+    DOCKER_TAG_COMMAND="docker tag $IMAGE_NAME us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/$IMAGE_NAME"
+    DOCKER_PUSH_COMMAND="docker push us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/$IMAGE_NAME"
+    DOCKER_PULL_COMMAND="docker pull us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/$IMAGE_NAME"
+
+    echo "On branch: ${GIT_BRANCH}"
+    echo $DOCKER_BUILD_COMMAND
+    eval $DOCKER_BUILD_COMMAND
+    echo $DOCKER_TAG_COMMAND
+    eval $DOCKER_TAG_COMMAND
+    echo $DOCKER_PUSH_COMMAND
+    eval $DOCKER_PUSH_COMMAND
+    echo "To pull container run: "
+    echo $DOCKER_PULL_COMMAND
+done
