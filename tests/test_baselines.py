bd07237b1d551293d6f441bb18396f66e994b30d
Priya Kasimbeg <kasimbeg@google.com>
2023-06-28 20:58:35 +0000

    revert

--- tests/test_baselines.py
+++ tests/test_baselines.py
@@ -15,7 +15,7 @@ from absl.testing import parameterized
 from algorithmic_efficiency.profiler import PassThroughProfiler
 import submission_runner
 
-# FLAGS = flags.FLAGS
+FLAGS = flags.FLAGS
 # Needed to avoid UnparsedFlagAccessError
 # (see https://github.com/google/model_search/pull/8).
 # FLAGS(sys.argv)
@@ -67,7 +67,7 @@ class BaselineTest(parameterized.TestCase):
                                framework,
                                submission_path,
                                tuning_search_space):
-    # FLAGS.framework = framework
+    FLAGS.framework = framework
     workload_metadata = copy.deepcopy(submission_runner.WORKLOADS[workload])
     workload_metadata['workload_path'] = os.path.join(
         submission_runner.BASE_WORKLOADS_DIR,
@@ -88,7 +88,7 @@ class BaselineTest(parameterized.TestCase):
         profiler=PassThroughProfiler(),
         max_global_steps=MAX_GLOBAL_STEPS,
     )
-    # logging.info(score)
+    logging.info(score)
 
 
 if __name__ == '__main__':
