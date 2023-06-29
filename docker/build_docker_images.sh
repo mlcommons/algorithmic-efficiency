af2e5b52be750865dae6881d1d3f99eb18f95182
Priya Kasimbeg <kasimbeg@google.com>
2023-06-29 17:36:42 +0000

    change imagenames

--- docker/build_docker_images.sh
+++ docker/build_docker_images.sh
@@ -16,8 +16,8 @@ fi
 
 for FRAMEWORK in "jax" "pytorch" "both"
 do
-    IMAGE_NAME="algoperf_$FRAMEWORK:$GIT_BRANCH"
-    DOCKER_BUILD_COMMAND="docker build --no-cache -t $IMAGE_NAME . --build-arg framework=$FRAMEWORK --build-arg branch=dockerfile_framework_arg"
+    IMAGE_NAME="algoperf_${FRAMEWORK}_${GIT_BRANCH}"
+    DOCKER_BUILD_COMMAND="docker build --no-cache -t $IMAGE_NAME . --build-arg framework=$FRAMEWORK --build-arg branch=$GIT_BRANCH
     DOCKER_TAG_COMMAND="docker tag $IMAGE_NAME us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/$IMAGE_NAME"
     DOCKER_PUSH_COMMAND="docker push us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/$IMAGE_NAME"
     DOCKER_PULL_COMMAND="docker pull us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/$IMAGE_NAME"
