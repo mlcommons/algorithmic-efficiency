57e59d35fad961d3e0ec4682116b1fd9dee41bb8
priyakasimbeg <priyakasimbeg@users.noreply.github.com>
2023-07-12 21:19:45 +0000

    add metadata to e2e scoring script

--- scoring/score_submission.py
+++ scoring/score_submission.py
@@ -4,6 +4,7 @@ from absl import logging
 
 import scoring_utils 
 import scoring
+from algorithmic_efficiency import workloads
 
 flags.DEFINE_string('experiment_path',
                     None,
@@ -11,13 +12,87 @@ flags.DEFINE_string('experiment_path',
 flags.DEFINE_string('submission_tag',
                     'my.submission',
                     'Submission tag.')
+flags.DEFINE_string('output_dir',
+                    'scoring_results',
+                    'Path to save performance profile table and plot.')
 FLAGS = flags.FLAGS
 
 workload_metadata = {
     'mnist_jax': {
-        'target': 0.8,
+        'target': workloads.mnist.workload.BaseMnistWorkload.validation_target_value,
         'metric': 'validation/accuracy'
     }
+    'mnist_pytorch': {
+        'target': workloads.mnist.workload.BaseMnistWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'criteo1tb_jax': {
+        'target': workloads.criteo1tb.workload.BaseCriteo1TbDlrmSmallWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'criteo1tb_pytorch': {
+        'target': workloads.criteo1tb.workload.BaseCriteo1TbDlrmSmallWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'fastmri_jax': {
+        'target': workloads.fastmri.workload.BaseFastMRIWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'fastmri_pytorch': {
+        'target': workloads.fastmri.workload.BaseFastMRIWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'imagenet_resnet': {
+        'target': workloads.criteo1tb.workload.BaseCriteo1TbDlrmSmallWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'imagenet_resnet': {
+        'target': workloads.criteo1tb.workload.BaseCriteo1TbDlrmSmallWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'imagenet_vit': {
+        'target': workloads.criteo1tb.workload.BaseCriteo1TbDlrmSmallWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'imagenet_vit': {
+        'target': workloads.criteo1tb.workload.BaseCriteo1TbDlrmSmallWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'librispeech_conformer': {
+        'target': workloads.criteo1tb.workload.BaseCriteo1TbDlrmSmallWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'librispeech_conformer': {
+        'target': workloads.criteo1tb.workload.BaseCriteo1TbDlrmSmallWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'librispeech_deepspeech': {
+        'target': workloads.criteo1tb.workload.BaseCriteo1TbDlrmSmallWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'librispeech_deepspeech': {
+        'target': workloads.criteo1tb.workload.BaseCriteo1TbDlrmSmallWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'ogbg': {
+        'target': workloads.criteo1tb.workload.BaseCriteo1TbDlrmSmallWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'ogbg': {
+        'target': workloads.criteo1tb.workload.BaseCriteo1TbDlrmSmallWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'wmt': {
+        'target': workloads.criteo1tb.workload.BaseCriteo1TbDlrmSmallWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+    'wmt': {
+        'target': workloads.criteo1tb.workload.BaseCriteo1TbDlrmSmallWorkload.validation_target_value,
+        'metric': 'validation/accuracy'
+    }
+
+
+
 }
         
 
@@ -36,6 +111,10 @@ def main(_):
                                                         num_points=100,
                                                         scale='linear',
                                                         verbosity=0)
+    plot_performance_profiles(performance_profile_df,
+                              'score',
+                              save_dir=FLAGS.ouptut_dir)
+    
     logging.info(performance_profile_df)
 
 
