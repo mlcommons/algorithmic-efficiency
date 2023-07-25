d6baa711079c38c023bbb572beb5fc3c9366cf4e
Priya Kasimbeg <kasimbeg@google.com>
2023-07-25 19:31:55 +0000

    debugging statemetns

--- submission_runner.py
+++ submission_runner.py
@@ -312,6 +312,9 @@ def train_once(
     global_step += 1
     if (max_global_steps is not None) and (global_step == max_global_steps):
       train_state['training_complete'] = True
+    
+    print('training complete')
+    print(train_state['training_complete'])
 
     train_step_end_time = get_time()
 
@@ -320,6 +323,8 @@ def train_once(
     train_state['is_time_remaining'] = (
         train_state['accumulated_submission_time'] <
         workload.max_allowed_runtime_sec)
+    print('is time remaining')
+    print(train_state['is_time_remaining'])
     # Check if submission is eligible for an untimed eval.
     if ((train_step_end_time - train_state['last_eval_time']) >=
         workload.eval_period_time_sec or train_state['training_complete']):
