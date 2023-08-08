ef0ece994d2dc51e8a8acee6735afb7ea9559552
Priya Kasimbeg <kasimbeg@google.com>
2023-08-08 01:46:38 +0000

    fix entrypoint documentation

--- docker/scripts/test_startup.sh
+++ docker/scripts/test_startup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-"""
-Test for startup script for docker startup script.
 
-Usage:
-    bash algorithmic-efficiency/docker/scripts/test_startup.sh
-"""
+# Test for startup script for docker startup script.
+#
+# Usage:
+#     bash algorithmic-efficiency/docker/scripts/test_startup.sh
 
 
 test_startup_script_short_flags() {
@@ -14,11 +13,12 @@ test_startup_script_short_flags() {
     -s baselines/adamw/jax/submission.py \
     -w mnist \
     -t baselines/adamw/tuning_search_space.json \
-    -e test_today/adamw \
+    -e test_docker_entrypoint/adamw \
     -m 10 \
     -c false \
     -o true \
     -r false \
+    -h $HOME
     "
     echo $command
     eval $command
@@ -31,11 +31,12 @@ test_startup_script_long_flags() {
     --submission_path baselines/adamw/jax/submission.py \
     --workload mnist \
     --tuning_search_space baselines/adamw/tuning_search_space.json \
-    --experiment_name test_today/adamw \
+    --experiment_name test_docker_entrypoint/adamw \
     --max_global_steps 10 \
     --save_checkpoints False \
     --overwrite true 
-    --rsync_data false"
+    --rsync_data false
+    --home_dir $HOME"
     echo $command
     eval $command
 }
