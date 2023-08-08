df83db57196a9b0357845cf7881f4bf14f3a4e40
Priya Kasimbeg <kasimbeg@google.com>
2023-08-08 01:26:29 +0000

    add unit tests for docker startup script

---
+++ docker/scripts/test_startup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+"""
+Test for startup script for docker startup script.
+
+Usage:
+    bash algorithmic-efficiency/docker/scripts/test_startup.sh
+"""
+
+
+test_startup_script_short_flags() {
+    command="bash algorithmic-efficiency/docker/scripts/startup.sh \
+    -d mnist \
+    -f jax \
+    -s baselines/adamw/jax/submission.py \
+    -w mnist \
+    -t baselines/adamw/tuning_search_space.json \
+    -e test_today/adamw \
+    -m 10 \
+    -c false \
+    -o true \
+    -r false \
+    "
+    echo $command
+    eval $command
+}
+
+test_startup_script_long_flags() {
+    command="bash algorithmic-efficiency/docker/scripts/startup.sh 
+    --dataset mnist \
+    --framework jax \
+    --submission_path baselines/adamw/jax/submission.py \
+    --workload mnist \
+    --tuning_search_space baselines/adamw/tuning_search_space.json \
+    --experiment_name test_today/adamw \
+    --max_global_steps 10 \
+    --save_checkpoints False \
+    --overwrite true 
+    --rsync_data false"
+    echo $command
+    eval $command
+}
+
+test_startup_script_short_flags
+test_startup_script_long_flags
+
