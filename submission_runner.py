d21c94bc2ed7a414ce4a3b38a0f699b7e561b001
Juhan Bae <pomonam15@gmail.com>
2023-08-14 11:00:27 -0400

    Deepspeech fix

--- submission_runner.py
+++ submission_runner.py
@@ -192,8 +192,8 @@ def train_once(
     model_params, model_state = workload.init_model_fn(
         model_init_rng, dropout_rate, aux_dropout_rate)
     if FLAGS.framework == 'pytorch' and FLAGS.torch_compile:
-      compile_error_workloads = ['ogbg', 'librispeech_deepspeech', 'wmt']
-      eager_backend_workloads = ['librispeech_conformer']
+      compile_error_workloads = ['ogbg', 'wmt']
+      eager_backend_workloads = ['librispeech_conformer', 'librispeech_deepspeech']
       aot_eager_backend_workloads = ['criteo1tb']
       if FLAGS.workload in compile_error_workloads:
         logging.warning(
