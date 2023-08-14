6d027596070c8febeb3c87ca6402589d63e2b7f5
Juhan Bae <pomonam15@gmail.com>
2023-08-14 11:51:58 -0400

    Remove recompile

--- submission_runner.py
+++ submission_runner.py
@@ -389,9 +389,6 @@ def train_once(
 
           if FLAGS.framework == 'pytorch' and torch.cuda.is_available():
             # Clean up the GPU cache after evaluation.
-            # See https://github.com/pytorch/pytorch/issues/99835.
-            torch._C._cuda_clearCublasWorkspaces()
-            torch._dynamo.reset()
             gc.collect()
             torch.cuda.empty_cache()
             logging.info(f'Released all unoccupied cached memory.')
