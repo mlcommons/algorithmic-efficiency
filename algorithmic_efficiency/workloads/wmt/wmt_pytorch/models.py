49292e48f146e0308df6f50beb3283dac40e5eb3
runame <re393@cam.ac.uk>
2023-08-15 16:53:30 +0200

    Fix for old yapf version

--- algorithmic_efficiency/workloads/wmt/wmt_pytorch/models.py
+++ algorithmic_efficiency/workloads/wmt/wmt_pytorch/models.py
@@ -939,8 +939,8 @@ class MultiheadAttention(nn.Module):
       # not the remaining zero elements.
       if attn_mask is not None:
         raise ValueError('Attention mask has to be None for decode == True.')
-      attn_mask = (torch.arange(max_length, device=k.device)
-                   >= cache_index).reshape(1, max_length)
+      attn_mask = (torch.arange(max_length, device=k.device) >=
+                   cache_index).reshape(1, max_length)
 
     # Update sequence length to account for complete sequence.
     seq_len = k.size(1)
