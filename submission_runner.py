68592659c253f669831d3ff728f6fd6c6287a1d1
Juhan Bae <pomonam15@gmail.com>
2023-08-24 17:04:09 -0400

    Disable com

--- submission_runner.py
+++ submission_runner.py
@@ -200,11 +200,11 @@ def train_once(
     model_params, model_state = workload.init_model_fn(
         model_init_rng, dropout_rate, aux_dropout_rate)
     if FLAGS.framework == 'pytorch' and FLAGS.torch_compile:
-      compile_error_workloads = ['ogbg']
+      compile_error_workloads = ['ogbg', 'criteo1tb']
       eager_backend_workloads = [
           'librispeech_conformer', 'librispeech_deepspeech'
       ]
-      aot_eager_backend_workloads = ['criteo1tb']
+      aot_eager_backend_workloads = []
       if FLAGS.workload in compile_error_workloads:
         logging.warning(
             'These workloads cannot be fully compiled under current '
