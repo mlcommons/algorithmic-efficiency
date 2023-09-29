3c72358535048ae7d846740efe5fe61fd7ec93fa
Priya Kasimbeg <kasimbeg@google.com>
2023-09-29 21:01:13 +0000

    fix

--- algorithmic_efficiency/workloads/librispeech_deepspeech/workload.py
+++ algorithmic_efficiency/workloads/librispeech_deepspeech/workload.py
@@ -1,4 +1,4 @@
-from algorithmic_efficiency.workloads.librispeech_conformer import workload
+from algorithmic_efficiency.workloads.librispeech_deepspeech import workload
 
 
 class BaseDeepspeechLibrispeechWorkload(workload.BaseLibrispeechWorkload):
