7a764e145a00ca19d5f278b70706f37ced09649e
Priya Kasimbeg <kasimbeg@google.com>
2023-10-07 00:55:53 +0000

    move env var

--- submission_runner.py
+++ submission_runner.py
@@ -49,6 +49,7 @@ tf.config.set_visible_devices([], 'GPU')
 
 # disable only for deepspeech if it works fine for other workloads.
 os.environ['XLA_FLAGS'] = '--xla_gpu_enable_triton_gemm=false'
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:256'
 
 # TODO(znado): make a nicer registry of workloads that lookup in.
 BASE_WORKLOADS_DIR = workloads.BASE_WORKLOADS_DIR
