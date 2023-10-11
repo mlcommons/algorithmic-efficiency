4d851cea6fc2a480c313be96100702808ae2ab5f
Chandramouli Shama Sastry <chandramouli.sastry@gmail.com>
2023-10-11 17:42:11 +0000

    scoring tests

--- scoring/test_scoring_utils.py
+++ scoring/test_scoring_utils.py
@@ -1,5 +1,6 @@
 from absl.testing import absltest
 import scoring_utils
+import scoring 
 
 TEST_LOGFILE = 'test_data/adamw_fastmri_jax_04-18-2023-13-10-58.log'
 TEST_DIR = 'test_data/experiment_dir'
@@ -25,6 +26,19 @@ class Test(absltest.TestCase):
 
   def test_get_experiment_df(self):
     df = scoring_utils.get_experiment_df(TEST_DIR)
+    assert len(df)==2
+
+  def test_scores(self):
+    df = scoring_utils.get_experiment_df(TEST_DIR)
+    performance_profile_df = scoring.compute_performance_profiles(
+      {'my.submission':df},
+      time_col='score',
+      min_tau=1.0,
+      max_tau=None,
+      reference_submission_tag=None,
+      num_points=100,
+      scale='linear',
+      verbosity=0)
 
 
 if __name__ == '__main__':
