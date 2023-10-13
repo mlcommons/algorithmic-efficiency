d12ee57d5544151fe889fc4faccf136b86dde7d5
Priya Kasimbeg <kasimbeg@google.com>
2023-10-13 20:37:20 +0000

    update fastmri targets

--- algorithmic_efficiency/workloads/fastmri/workload.py
+++ algorithmic_efficiency/workloads/fastmri/workload.py
@@ -19,14 +19,14 @@ class BaseFastMRIWorkload(spec.Workload):
 
   @property
   def validation_target_value(self) -> float:
-    return 0.7344
+    return 0.727120
 
   def has_reached_test_target(self, eval_result: float) -> bool:
     return eval_result['test/ssim'] > self.test_target_value
 
   @property
   def test_target_value(self) -> float:
-    return 0.741652
+    return 0.744295
 
   @property
   def loss_type(self) -> spec.LossType:
