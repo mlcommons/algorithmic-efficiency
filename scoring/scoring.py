8a4f8fbb18ddfee676713cb6abfebfe5f32333e9
Priya Kasimbeg <kasimbeg@google.com>
2023-10-24 00:40:58 +0000

    debugging

--- scoring/scoring.py
+++ scoring/scoring.py
@@ -355,6 +355,7 @@ def plot_performance_profiles(perf_df,
   Returns:
     None. If a valid save_dir is provided, save both the plot and perf_df.
   """
+  print(perf_df)
   fig = perf_df.T.plot(figsize=figsize)
   df_col_display = f'log10({df_col})' if scale == 'log' else df_col
   fig.set_xlabel(
