95f252a5216d8e8472befcb97fac08325e5420d8
andres-fr <fr_andres@msn.com>
2023-11-02 11:57:17 +0100

    replaced double quotes with single quotes in script

--- docker/scripts/singularity_converter.py
+++ docker/scripts/singularity_converter.py
@@ -17,21 +17,21 @@ from spython.main.parse.parsers import get_parser
 from spython.main.parse.writers import get_writer
 
 # globals
-ENTRY_POINT = "/bin/bash"  # seems to be a good default
+ENTRY_POINT = '/bin/bash'  # seems to be a good default
 FORCE = False  # seems to be a good default
 #
-parser = argparse.ArgumentParser(description="Custom Singularity converter")
+parser = argparse.ArgumentParser(description='Custom Singularity converter')
 parser.add_argument('-i', '--input', type=str,
-                    help="Docker input path", default="Dockerfile")
+                    help='Docker input path', default='Dockerfile')
 parser.add_argument('-o', '--output', type=str,
-                    help="Singularity output path", default="Singularity.def")
+                    help='Singularity output path', default='Singularity.def')
 args = parser.parse_args()
 INPUT_DOCKERFILE_PATH = args.input
 OUTPUT_SINGULARITY_PATH = args.output
 
 # create Docker parser and Singularity writer
-parser = get_parser("docker")
-writer = get_writer("singularity")
+parser = get_parser('docker')
+writer = get_writer('singularity')
 
 # parse Dockerfile into Singularity and suppress %files commands
 recipeParser = parser(INPUT_DOCKERFILE_PATH)
@@ -41,5 +41,5 @@ recipeWriter.recipe[key].files = []
 
 # convert to string and save to output file
 result = recipeWriter.convert(runscript=ENTRY_POINT, force=FORCE)
-with open(OUTPUT_SINGULARITY_PATH, "w") as f:
+with open(OUTPUT_SINGULARITY_PATH, 'w') as f:
     f.write(result)
