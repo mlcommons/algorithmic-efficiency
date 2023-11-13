ca5d8e37a29d16d55fa709565622c66f8e67f6d8
andres-fr <fr_andres@msn.com>
2023-11-13 15:29:17 +0100

    solved isort linting issue in singularity_converter.py

--- docker/scripts/singularity_converter.py
+++ docker/scripts/singularity_converter.py
@@ -11,35 +11,40 @@ python singularity_converter.py -i Dockerfile -o Singularity.def
 
 
 import argparse
-#
-import spython
+
 from spython.main.parse.parsers import get_parser
 from spython.main.parse.writers import get_writer
 
 # globals
-ENTRY_POINT = '/bin/bash'  # seems to be a good default
+ENTRY_POINT = "/bin/bash"  # seems to be a good default
 FORCE = False  # seems to be a good default
 #
-parser = argparse.ArgumentParser(description='Custom Singularity converter')
-parser.add_argument('-i', '--input', type=str,
-                    help='Docker input path', default='Dockerfile')
-parser.add_argument('-o', '--output', type=str,
-                    help='Singularity output path', default='Singularity.def')
+parser = argparse.ArgumentParser(description="Custom Singularity converter")
+parser.add_argument(
+    "-i", "--input", type=str, help="Docker input path", default="Dockerfile"
+)
+parser.add_argument(
+    "-o",
+    "--output",
+    type=str,
+    help="Singularity output path",
+    default="Singularity.def",
+)
 args = parser.parse_args()
 INPUT_DOCKERFILE_PATH = args.input
 OUTPUT_SINGULARITY_PATH = args.output
 
 # create Docker parser and Singularity writer
-parser = get_parser('docker')
-writer = get_writer('singularity')
+parser = get_parser("docker")
+writer = get_writer("singularity")
 
 # parse Dockerfile into Singularity and suppress %files commands
 recipeParser = parser(INPUT_DOCKERFILE_PATH)
 recipeWriter = writer(recipeParser.recipe)
-key, = recipeParser.recipe.keys()
+(key,) = recipeParser.recipe.keys()
 recipeWriter.recipe[key].files = []
 
 # convert to string and save to output file
 result = recipeWriter.convert(runscript=ENTRY_POINT, force=FORCE)
-with open(OUTPUT_SINGULARITY_PATH, 'w') as f:
+with open(OUTPUT_SINGULARITY_PATH, "w") as f:
     f.write(result)
