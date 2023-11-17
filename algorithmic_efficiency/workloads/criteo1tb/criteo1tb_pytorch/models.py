0723d825f5af84e8f2b74f2a7435cf797b6e0d5c
priyakasimbeg <priyakasimbeg@users.noreply.github.com>
2023-11-17 02:35:28 +0000

    dlrm fix

--- algorithmic_efficiency/workloads/criteo1tb/criteo1tb_pytorch/models.py
+++ algorithmic_efficiency/workloads/criteo1tb/criteo1tb_pytorch/models.py
@@ -97,7 +97,7 @@ class DLRMResNet(nn.Module):
     self.dot_interact = DotInteract(num_sparse_features=num_sparse_features,)
 
     # TODO: Write down the formula here instead of the constant.
-    input_dims = 506
+    input_dims = 634
     top_mlp_layers = []
     num_layers_top = len(self.mlp_top_dims)
     for layer_idx, fan_out in enumerate(self.mlp_top_dims):
@@ -216,7 +216,7 @@ class DlrmSmall(nn.Module):
     self.dot_interact = DotInteract(num_sparse_features=num_sparse_features,)
 
     # TODO: Write down the formula here instead of the constant.
-    input_dims = 634
+    input_dims = 506
     top_mlp_layers = []
     num_layers_top = len(self.mlp_top_dims)
     for layer_idx, fan_out in enumerate(self.mlp_top_dims):
