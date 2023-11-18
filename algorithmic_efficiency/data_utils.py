49e221b29449c42d938aebae8fdfe483dc82661d
root <root@kasimbeg-2.europe-west4-a.c.training-algorithms-external.internal>
2023-11-18 07:42:42 +0000

    fix

--- algorithmic_efficiency/data_utils.py
+++ algorithmic_efficiency/data_utils.py
@@ -44,15 +44,11 @@ def shard_and_maybe_pad_np(
     targets = batch['targets']
     targets_shape = tuple(
         targets[0].shape if isinstance(targets, tuple) else targets.shape)
-    logging.info(f"TARGETS shape: {targets_shape}")
     # We need a 2d mask for WMT.
     mask_shape = targets_shape if len(targets_shape) < 3 else targets_shape[0]
     # Get weights from batch if there are any.
     weights = batch.get('weights')
     # The weights will also be padded.
-    for k, v in batch:
-      print(k)
-      print(np.shape(v))
     batch['weights'] = np.ones(mask_shape) if weights is None else weights
 
   def _prepare(x):
