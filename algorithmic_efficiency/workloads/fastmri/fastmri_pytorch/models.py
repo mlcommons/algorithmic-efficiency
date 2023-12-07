6c9c86fcfdbec87cc8d33b26a7bcd536bb158586
priyakasimbeg <priyakasimbeg@users.noreply.github.com>
2023-12-07 07:45:46 +0000

    fix

--- algorithmic_efficiency/workloads/fastmri/fastmri_pytorch/models.py
+++ algorithmic_efficiency/workloads/fastmri/fastmri_pytorch/models.py
@@ -146,11 +146,11 @@ class ConvBlock(nn.Module):
       activation_fn = nn.LeakyReLU(negative_slope=0.2, inplace=True)
     self.conv_layers = nn.Sequential(
         nn.Conv2d(in_chans, out_chans, kernel_size=3, padding=1, bias=False),
-        norm_layer(out_chans),
+        nn.GroupNorm(num_groups=1, num_channels=out_chans, eps=1e-6),
         activation_fn,
         nn.Dropout2d(dropout_rate),
         nn.Conv2d(out_chans, out_chans, kernel_size=3, padding=1, bias=False),
-        norm_layer(out_chans),
+        nn.GroupNorm(num_groups=1, num_channels=out_chans, eps=1e-6),
         activation_fn,
         nn.Dropout2d(dropout_rate),
     )
@@ -173,7 +173,7 @@ class TransposeConvBlock(nn.Module):
     super().__init__()
     if use_layer_norm:
       size = int(size)
-      norm_layer = partial(nn.GroupNorm, num_groups=1, eps=1e-6)
+      norm_layer = nn.GroupNorm(num_groups=1, num_channels=out_chans, eps=1e-6)
     else:
       norm_layer = nn.InstanceNorm2d
     if use_tanh:
@@ -183,7 +183,7 @@ class TransposeConvBlock(nn.Module):
     self.layers = nn.Sequential(
         nn.ConvTranspose2d(
             in_chans, out_chans, kernel_size=2, stride=2, bias=False),
-        norm_layer(out_chans),
+        nn.GroupNorm(num_groups=1, num_channels=out_chans, eps=1e-6),
         activation_fn,
     )
 
