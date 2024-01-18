454f50183a9ed5f152767790b3b95245fce39543
priyakasimbeg <priyakasimbeg@users.noreply.github.com>
2024-01-18 23:05:55 +0000

    add resnet variant target

--- algorithmic_efficiency/workloads/imagenet_resnet/imagenet_pytorch/workload.py
+++ algorithmic_efficiency/workloads/imagenet_resnet/imagenet_pytorch/workload.py
@@ -336,3 +336,11 @@ class ImagenetResNetLargeBNScaleWorkload(ImagenetResNetWorkload):
   @property
   def bn_init_scale(self) -> float:
     return 8.0
+  
+  @property
+  def validation_target_value(self) -> float:
+    return 0.23474
+
+  @property
+  def test_target_value(self) -> float:
+    return 0.3577
