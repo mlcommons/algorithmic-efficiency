18bc3474c158b63b32991306bc06553520d1e84d
Priya Kasimbeg <kasimbeg@google.com>
2024-01-26 18:50:55 +0000

    remove debugging statemetns

--- docker/scripts/startup.sh
+++ docker/scripts/startup.sh
@@ -209,8 +209,6 @@ if [[ ! -z ${SUBMISSION_PATH+x} ]]; then
     fi
 
     if [[ ! -z ${HPARAM_END_INDEX+x} ]]; then 
-        echo "SETTING FLAGGGGGG"
-        echo ${HPARAM_END_INDEX}
         HPARAM_END_INDEX_FLAG="--hparam_end_index=${HPARAM_END_INDEX}"
     fi
 
