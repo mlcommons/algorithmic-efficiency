3494f16c79784c27de1974ece9d0ff706496f09f
Priya Kasimbeg <kasimbeg@google.com>
2024-02-03 01:31:28 +0000

    fix overflow error in jax sampling

--- algorithmic_efficiency/random_utils.py
+++ algorithmic_efficiency/random_utils.py
@@ -86,8 +86,8 @@ def PRNGKey(seed: SeedType) -> SeedType:  # pylint: disable=invalid-name
   return _PRNGKey(seed)
 
 
-def randint(seed: SeedType) -> int:
+def bits(seed: SeedType) -> int:
   if FLAGS.framework == 'jax':
     _check_jax_install()
-    return jax_rng.randint(seed, (), 0, MAX_INT32)
+    return jax_rng.bits(seed)
   return _randint(seed)
