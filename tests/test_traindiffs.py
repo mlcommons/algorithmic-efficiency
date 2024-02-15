7f8452ec7c90544480741e7063741d7ca4413288
Priya Kasimbeg <kasimbeg@google.com>
2024-02-15 01:14:22 +0000

    print errors in subprocesses

--- tests/test_traindiffs.py
+++ tests/test_traindiffs.py
@@ -50,20 +50,26 @@ class ModelDiffTest(parameterized.TestCase):
     name = f'Testing {workload}'
     jax_logs = '/tmp/jax_log.pkl'
     pyt_logs = '/tmp/pyt_log.pkl'
-    run(
-        f'python3 -m tests.reference_algorithm_tests --workload={workload} --framework=jax --global_batch_size={GLOBAL_BATCH_SIZE} --log_file={jax_logs}'
-        f' --submission_path=tests/modeldiffs/vanilla_sgd_jax.py --identical=True --tuning_search_space=None --num_train_steps={NUM_TRAIN_STEPS}',
-        shell=True,
-        stdout=DEVNULL,
-        stderr=STDOUT,
-        check=True)
-    run(
-        f'torchrun --standalone --nnodes 1 --nproc_per_node 8 -m tests.reference_algorithm_tests --workload={workload} --framework=pytorch --global_batch_size={GLOBAL_BATCH_SIZE} --log_file={pyt_logs}'
-        f' --submission_path=tests/modeldiffs/vanilla_sgd_pytorch.py --identical=True --tuning_search_space=None --num_train_steps={NUM_TRAIN_STEPS}',
-        shell=True,
-        stdout=DEVNULL,
-        stderr=STDOUT,
-        check=True)
+    try: 
+      run(
+          f'python3 -m tests.reference_algorithm_tests --workload={workload} --framework=jax --global_batch_size={GLOBAL_BATCH_SIZE} --log_file={jax_logs}'
+          f' --submission_path=tests/modeldiffs/vanilla_sgd_jax.py --identical=True --tuning_search_space=None --num_train_steps={NUM_TRAIN_STEPS}',
+          shell=True,
+          stdout=DEVNULL,
+          stderr=STDOUT,
+          check=True)
+    except subprocess.CalledProcessError as e:
+      print("Error:", e)
+    try: 
+      run(
+          f'torchrun --standalone --nnodes 1 --nproc_per_node 8 -m tests.reference_algorithm_tests --workload={workload} --framework=pytorch --global_batch_size={GLOBAL_BATCH_SIZE} --log_file={pyt_logs}'
+          f' --submission_path=tests/modeldiffs/vanilla_sgd_pytorch.py --identical=True --tuning_search_space=None --num_train_steps={NUM_TRAIN_STEPS}',
+          shell=True,
+          stdout=DEVNULL,
+          stderr=STDOUT,
+          check=True)
+    except subprocess.CalledProcessError as e:
+      print("Error:", e)
     with open(jax_logs, 'rb') as f:
       jax_results = pickle.load(f)
     with open(pyt_logs, 'rb') as f:
