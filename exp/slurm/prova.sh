9531d70556db590e5fc2382aab0f7a47c03e2b41
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2024-02-16 12:50:09 +0100

    added prova

---
+++ exp/slurm/prova.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=imagenet_resnet_slurm_check
+#SBATCH --output=~/log/algoperf/job.%j.%N.out
+#SBATCH --error=~/log/algoperf/job.%j.%N.err
+
+#SBATCH --time=1:00:00
+
+#SBATCH --ntasks 1
+#SBATCH --requeue
+#SBATCH --cpus-per-task 16
+#SBATCH --mem=200000M
+
+# Get node with GPUs
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
+
+
+LOG_DIR=~/log/algoperf
+
+srun ~/algorithmic-efficiency/exp/shell/imagenet_resnet_slurm.sh  > $LOG_DIR/job.%j.%N.log
---
+++ exp/slurm/prova_exe.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Activate conda environment TODO: should I use source activate alpe instead?
+conda activate alpe
+
+echo "ECCOCI"
