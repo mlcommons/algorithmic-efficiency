41059fc4b634e0132881ffeebc96297106f1ba6d
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2024-02-19 16:29:44 +0100

    minor changes

--- exp/slurm/mnist_sub.sh
+++ exp/slurm/mnist_sub.sh
@@ -4,17 +4,16 @@
 #SBATCH --output=/ptmp/najroldi/logs/algoperf/job_%j.out
 #SBATCH --error=/ptmp/najroldi/logs/algoperf/job_%j.err
 
-#SBATCH --time=00:10:00
+#SBATCH --time=00:5:00
 #SBATCH --ntasks 1
 #SBATCH --requeue
 
-#SBATCH --cpus-per-task 4
+#SBATCH --cpus-per-task 2
 #SBATCH --mem=1000M
 
 # Get node with GPUs
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --constraint="gpu&^gpu-bw"
 # the last constraint ensures that we are not reserving gpu-bw
 
 srun ~/algorithmic-efficiency/exp/shell/mnist.sh
