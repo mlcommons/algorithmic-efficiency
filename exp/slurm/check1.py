6146655dcf182e2af75ed28b4a37242420e9bb83
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2024-02-20 09:56:56 +0100

    pylint adjustment

--- exp/slurm/check1.py
+++ exp/slurm/check1.py
@@ -2,14 +2,14 @@ import psutil
 
 
 def check_disk_io_counters_support():
-    try:
-        disk_io = psutil.disk_io_counters()
-        if disk_io is None:
-            print("disk_io_counters() is not supported on this machine.")
-        else:
-            print("disk_io_counters() is supported on this machine.")
-            print("Disk I/O stats:", disk_io)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+  try:
+    disk_io = psutil.disk_io_counters()
+    if disk_io is None:
+      print("disk_io_counters() is not supported on this machine.")
+    else:
+      print("disk_io_counters() is supported on this machine.")
+      print("Disk I/O stats:", disk_io)
+  except psutil.Error as e:
+    print(f"An error occurred: {e}")
 
 check_disk_io_counters_support()
