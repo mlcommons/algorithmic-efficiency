2a62b380324eafc609146709ee62b6213f36ed4b
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2024-02-28 09:57:27 +0100

    mino change

--- submission_runner.py
+++ submission_runner.py
@@ -540,10 +540,11 @@ def score_submission_on_workload(workload: spec.Workload,
     with open(tuning_search_space, 'r', encoding='UTF-8') as search_space_file:
       tuning_search_space = halton.generate_search(
           json.load(search_space_file), num_tuning_trials)
-
+    
     if trial_index is not None:
       if trial_index < 1 or trial_index > num_tuning_trials:
         raise ValueError('trial_index should be in [1, num_tuning_trials]')
+    # halton.generate_search always produce the same list, but order may vary
     tuning_search_space.sort()
 
     all_timings = []
