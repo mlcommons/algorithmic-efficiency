96e0c02a5e632d8d8248093ee33966e2cdc96c6f
Priya Kasimbeg <kasimbeg@google.com>
2024-03-08 22:32:19 +0000

    undo workloads.py changes

--- algorithmic_efficiency/workloads/workloads.py
+++ algorithmic_efficiency/workloads/workloads.py
@@ -159,7 +159,6 @@ def get_base_workload_name(workload_name):
   for base_workload_name in BASE_WORKLOADS:
     if base_workload_name in workload_name:
       return base_workload_name
-  return workload_name
 
 
 def convert_filepath_to_module(path: str):
