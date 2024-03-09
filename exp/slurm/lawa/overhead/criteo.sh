f8de882326643a77def804f429b27397f5a3e19b
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2024-03-09 19:05:08 +0100

    first lawa exp 01

---
+++ exp/slurm/lawa/overhead/criteo.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#SBATCH --job-name=triang_03_criteo
+#SBATCH --array=1-25
+#SBATCH --error=/ptmp/najroldi/logs/algoperf/err/%x_%A_%a.err
+#SBATCH --output=/ptmp/najroldi/logs/algoperf/out/%x_%A_%a.out
+#SBATCH --time=04:00:00
+#SBATCH --ntasks 1
+#SBATCH --requeue
+# --- 4 GPUs on a full node ---
+#SBATCH --gres=gpu:a100:4
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=500000
+
+source ~/.bashrc
+conda activate alpe
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export CODE_DIR=~/algorithmic-efficiency
+export EXP_DIR=/ptmp/najroldi/exp/algoperf
+export DATA_DIR=/ptmp/najroldi/data
+
+# Workload
+dataset=criteo1tb
+workload=criteo1tb
+
+# Submission
+submission='submissions/nadamw_triang/nadamw_triang.py'
+search_space='submissions/nadamw_triang/space_1.json'
+# submission='submissions/nadamw_trapez/nadamw_trapez.py'
+# search_space='submissions/nadamw_trapez/space_3.json'
+
+# Experiment name, study
+base_name="nadamw_triang_01"
+# base_name="nadamw_trapez_03"
+study=1
+
+# Set config
+experiment_name="${base_name}/study_${study}"
+num_tuning_trials=$SLURM_ARRAY_TASK_MAX
+trial_index=$SLURM_ARRAY_TASK_ID
+rng_seed=3429199924 #$study # same seed across trials
+
+# Execute python script
+torchrun \
+  --redirects 1:0,2:0,3:0 \
+  --standalone \
+  --nnodes=1 \
+  --nproc_per_node=4 \
+  $CODE_DIR/submission_runner.py \
+  --workload=$workload \
+  --framework=pytorch \
+  --tuning_ruleset=external \
+  --data_dir=$DATA_DIR/$dataset \
+  --imagenet_v2_data_dir=$DATA_DIR/$dataset \
+  --submission_path=$submission \
+  --tuning_search_space=$search_space \
+  --num_tuning_trials=$num_tuning_trials \
+  --trial_index=$trial_index \
+  --rng_seed=$rng_seed \
+  --experiment_dir=$EXP_DIR  \
+  --experiment_name=$experiment_name \
+  --save_intermediate_checkpoints=False \
+  --resume_last_run \
+  --use_wandb \
+  --fixed_space
---
+++ exp/slurm/lawa/overhead/fastmri.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#SBATCH --job-name=fastmri
+#SBATCH --error=/ptmp/najroldi/logs/algoperf/err/%j.err
+#SBATCH --output=/ptmp/najroldi/logs/algoperf/out/%j.out
+#SBATCH --time=24:00:00
+#SBATCH --ntasks 1
+#SBATCH --requeue
+# --- 4 GPUs on a full node ---
+#SBATCH --gres=gpu:a100:4
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=500000
+
+source ~/.bashrc
+conda activate alpe
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export CODE_DIR=~/algorithmic-efficiency
+export EXP_DIR=/ptmp/najroldi/exp/algoperf
+export DATA_DIR=/ptmp/najroldi/data
+
+# Workload
+dataset=fastmri
+workload=fastmri
+
+# Submission
+submission='prize_qualification_baselines/external_tuning/pytorch_nadamw_target_setting.py'
+search_space='exp/slurm/lawa/overhead/baseline.json'
+
+# Experiment name, study
+base_name="lawa_overhead_baseline"
+study=1
+num_tuning_trials=1
+
+# Set experient name
+experiment_name="${base_name}/study_${study}"
+
+# Execute python script
+torchrun \
+  --redirects 1:0,2:0,3:0 \
+  --standalone \
+  --nnodes=1 \
+  --nproc_per_node=4 \
+  $CODE_DIR/submission_runner.py \
+  --workload=$workload \
+  --framework=pytorch \
+  --tuning_ruleset=external \
+  --data_dir=$DATA_DIR/$dataset \
+  --imagenet_v2_data_dir=$DATA_DIR/$dataset \
+  --submission_path=$submission \
+  --tuning_search_space=$search_space \
+  --num_tuning_trials=$num_tuning_trials \
+  --experiment_dir=$EXP_DIR  \
+  --experiment_name=$experiment_name \
+  --save_intermediate_checkpoints=False \
+  --resume_last_run \
+  --use_wandb \
+  --max_global_steps=1000 \
+  --rng_seed=1996
