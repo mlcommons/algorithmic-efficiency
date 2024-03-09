9220ceeb3a4599b7c4d46e9df9df61b8d1902248
Priya Kasimbeg <kasimbeg@google.com>
2024-03-09 04:44:55 +0000

    add test data back in

---
+++ scoring/test_performance_profile.py
@@ -0,0 +1,27 @@
+from absl.testing import absltest
+
+from scoring import performance_profile
+from scoring import scoring_utils
+
+import os
+
+TEST_LOGFILE = 'test_data/adamw_fastmri_jax_04-18-2023-13-10-58.log'
+TEST_DIR = 'test_data/experiment_dir'
+NUM_EVALS = 18
+
+
+class Test(absltest.TestCase):
+
+  def test_get_workloads_time_to_target(self):
+    pass
+
+  def test_get_best_trial_index(self):
+    pass
+
+  def test_get_trials_df(self):
+
+
+
+
+if __name__ == '__main__':
+  absltest.main()
