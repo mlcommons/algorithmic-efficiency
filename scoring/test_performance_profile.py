2700fc36cbde583f2dac6dc3bdf2f04ad28781d1
Priya Kasimbeg <kasimbeg@google.com>
2024-03-09 05:10:21 +0000

    add todos

--- scoring/test_performance_profile.py
+++ scoring/test_performance_profile.py
@@ -5,20 +5,19 @@ from absl.testing import absltest
 from scoring import performance_profile
 from scoring import scoring_utils
 
-TEST_LOGFILE = 'test_data/adamw_fastmri_jax_04-18-2023-13-10-58.log'
-TEST_DIR = 'test_data/experiment_dir'
-NUM_EVALS = 18
-
 
 class Test(absltest.TestCase):
 
   def test_get_workloads_time_to_target(self):
+    # TODO(kasimbeg)
     pass
 
   def test_get_best_trial_index(self):
+    # TODO(kasimbeg)
     pass
 
   def test_compute_performance_profiles(self):
+    # TODO(kasimbeg)
     pass
 
 
