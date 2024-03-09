9720950e9d5c9464bd90d11e0137b728a599ae90
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2024-03-09 20:21:12 +0100

    lawa oom test ran

---
+++ exp/condor/lawa/auto_run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate alpe
+
+# Env vars
+export OMP_NUM_THREADS=28
+export HOME=/home/najroldi
+export CODE_DIR=/home/najroldi/algorithmic-efficiency
+export EXP_DIR=/fast/najroldi/exp/algoperf
+export DATA_DIR=/fast/najroldi/data
+
+# Job specific vars
+dataset=$1
+workload=$2
+submission=$3
+search_space=$4
+name=$5
+study=$6
+num_tuning_trials=$7
+
+# Experiment name
+experiment_name="${name}/study_${study}"
+
+# Librispeech tokenizer path
+tokenizer_path=''
+if [ "$dataset" = "librispeech" ]; then
+    tokenizer_path="${DATA_DIR}/librispeech/spm_model.vocab"
+fi
+
+# Execute python script
+torchrun \
+  --redirects 1:0,2:0,3:0,4:0,5:0,6:0,7:0 \
+  --standalone \
+  --nnodes=1 \
+  --nproc_per_node=8 \
+  $CODE_DIR/submission_runner.py \
+  --workload=$workload \
+  --framework=pytorch \
+  --tuning_ruleset=external \
+  --data_dir=$DATA_DIR/$dataset \
+  --imagenet_v2_data_dir=$DATA_DIR/$dataset \
+  --librispeech_tokenizer_vocab_path=$tokenizer_path \
+  --submission_path=$submission \
+  --tuning_search_space=$search_space \
+  --num_tuning_trials=$num_tuning_trials \
+  --experiment_dir=$EXP_DIR  \
+  --experiment_name=$experiment_name \
+  --save_intermediate_checkpoints=False \
+  --resume_last_run \
+  --use_wandb \
+  --max_global_steps=50 \
+  --rng_seed=1996
