f208dd2dc13d98619633b3144e7300fd74060461
Juhan Bae <pomonam15@gmail.com>
2024-03-19 13:56:49 -0400

    Lint fix

--- algorithmic_efficiency/workloads/librispeech_conformer/librispeech_pytorch/models.py
+++ algorithmic_efficiency/workloads/librispeech_conformer/librispeech_pytorch/models.py
@@ -206,9 +206,9 @@ class FeedForwardModule(nn.Module):
     else:
       feed_forward_residual_dropout_rate = (
           config.feed_forward_residual_dropout_rate)
-    self.dropout2 = nn.Dropout(p=feed_forward_residual_dropout_rate,
-                               inplace=True)
-
+    self.dropout2 = nn.Dropout(
+        p=feed_forward_residual_dropout_rate, inplace=True)
+  
   def forward(self, inputs, padding_mask):
     inputs = self.ln(inputs)
     inputs = self.linear1(inputs)
