09e44a860ff86d2081ffc9731f6ebadb35f5b30c
Priya Kasimbeg <kasimbeg@google.com>
2024-03-20 21:11:28 +0000

    modify log directory structure for prize_qualification baselines

--- scoring/score_submissions.py
+++ scoring/score_submissions.py
@@ -57,20 +57,21 @@ def get_summary_df(workload, workload_df):
 
   summary_df = pd.DataFrame()
   summary_df['workload'] = workload_df['workload']
-  summary_df['trial'] = workload_df['trial']
+  summary_df['trial'] = workload_df['trial'].apply(
+    lambda x: x[0])
   summary_df['target metric name'] = validation_metric
   summary_df['target metric value'] = validation_target
 
   summary_df['target reached'] = workload_df[validation_metric].apply(
       lambda x: target_op(x, validation_target)).apply(np.any)
-  summary_df['best target'] = workload_df[validation_metric].apply(
+  summary_df['best metric value'] = workload_df[validation_metric].apply(
       lambda x: best_op(x))
   workload_df['index best eval'] = workload_df[validation_metric].apply(
       lambda x: idx_op(x))
-  summary_df['submission time (s)'] = workload_df.apply(
+  summary_df['time to best eval (s)'] = workload_df.apply(
       lambda x: x['accumulated_submission_time'][x['index best eval']], axis=1)
-  summary_df['submission time to target (s)'] = summary_df.apply(
-      lambda x: x['submission time (s)'] if x['target reached'] else np.inf,
+  summary_df['time to target (s)'] = summary_df.apply(
+      lambda x: x['time to best eval (s)'] if x['target reached'] else np.inf,
       axis=1)
 
   return summary_df
