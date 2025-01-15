ff4a457ea6eea6e2603887066e3e2735a8867d2a
Frank Schneider <frank.stefan.schneider@gmail.com>
2025-01-15 15:23:37 +0100

    Fix version test

--- tests/version_test.py
+++ tests/version_test.py
@@ -4,10 +4,13 @@ import algorithmic_efficiency
 
 
 def test_version_attribute():
-  """Check whether __version__ exists and is a valid string."""
+    """Check whether __version__ exists and is a valid string."""
 
-  assert hasattr(algorithmic_efficiency, "__version__")
-  version = algorithmic_efficiency.__version__
-  assert isinstance(version, str)
-  version_elements = version.split(".")
-  assert all(el.isnumeric() for el in version_elements)
+    assert hasattr(algorithmic_efficiency, "__version__")
+    version = algorithmic_efficiency.__version__
+    assert isinstance(version, str)
+    version_elements = version.split(".")
+    print(version_elements)
+    # Only check the first three elements, i.e. major, minor, patch.
+    # The remaining elements contain commit hash and dirty status.
+    assert all(el.isnumeric() for el in version_elements[0:3])
