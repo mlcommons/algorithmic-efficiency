7516a734a6db40fca06da3373e02dcbc773b7b61
Priya Kasimbeg <kasimbeg@google.com>
2025-03-21 23:37:04 +0000

    formatting

--- scoring/utils/slurm/make_job_config.py
+++ scoring/utils/slurm/make_job_config.py
@@ -19,12 +19,15 @@ TUNING_SEARCH_SPACE = None
 FRAMEWORK = 'pytorch'
 TUNING_RULESET = 'self'
 
-flags.DEFINE_string('submission_path',
-                    SUBMISSION_PATH,
-                    'Path to submission module relative to algorithmic-efficiency dir.')
-flags.DEFINE_string('tuning_search_space',
-                    TUNING_SEARCH_SPACE,
-                    'Path to tuning search space for submission module relative to algorithmic-efficiency dir.')
+flags.DEFINE_string(
+    'submission_path',
+    SUBMISSION_PATH,
+    'Path to submission module relative to algorithmic-efficiency dir.')
+flags.DEFINE_string(
+    'tuning_search_space',
+    TUNING_SEARCH_SPACE,
+    'Path to tuning search space for submission module relative to algorithmic-efficiency dir.'
+)
 flags.DEFINE_string('experiment_dir',
                     EXPERIMENT_DIR,
                     'Path to experiment dir where logs will be saved.')
