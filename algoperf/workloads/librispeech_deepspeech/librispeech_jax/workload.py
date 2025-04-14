06cce6e972b9bc4404375c44e4c42a00d3761dc2
Priya Kasimbeg <kasimbeg@google.com>
2025-04-14 23:17:52 +0000

    upgrade to 0.4.30

--- algoperf/workloads/librispeech_deepspeech/librispeech_jax/workload.py
+++ algoperf/workloads/librispeech_deepspeech/librispeech_jax/workload.py
@@ -51,8 +51,10 @@ class LibriSpeechDeepSpeechWorkload(LibriSpeechConformerWorkload):
     params = variables['params']
     self._param_shapes = param_utils.jax_param_shapes(params)
     self._param_types = param_utils.jax_param_types(self._param_shapes)
-    model_state = jax_utils.replicate(model_state)
-    params = jax_utils.replicate(params)
+    model_state = jax_utils.replicate(model_state, jax.devices())
+    params = jax_utils.replicate(params, jax.devices())
+    params_shapes = jax.tree.map(lambda x: jnp.shape(x), model_state)
+    jax.debug.print("params shapes {params_shapes}", params_shapes=params_shapes)
     return params, model_state
 
   def model_fn(
