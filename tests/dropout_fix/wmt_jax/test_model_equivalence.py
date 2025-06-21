8b0a12529870be76cd216835f943462424ec5131
Priya Kasimbeg <kasimbeg@google.com>
2025-06-21 08:28:09 +0000

    fix wmt test

--- tests/dropout_fix/wmt_jax/test_model_equivalence.py
+++ tests/dropout_fix/wmt_jax/test_model_equivalence.py
@@ -26,18 +26,20 @@ class ModeEquivalenceTest(parameterized.TestCase):
   @parameterized.named_parameters(
       dict(
           testcase_name='WMT, p=0.0',
-          dropout_rate=0.0),
+          dropout_rate=0.0,
+          train=True),
       dict(
           testcase_name='WMT p=0.1',
-          dropout_rate=0.1),
+          dropout_rate=0.1,
+          train=False),
   )
-  def test_forward(self, dropout_rate):
+  def test_forward(self, dropout_rate, train):
 
     # init model
     rng, data_rng, dropout_rng = jax.random.split(jax.random.key(SEED), 3)
 
-    orig_model = OrigCls(OrigClsConfig)
-    cust_model = CustCls(CustClsConfig)
+    orig_model = OrigCls(OrigClsConfig(deterministic=not train, attention_dropout_rate=dropout_rate, dropout_rate=dropout_rate))
+    cust_model = CustCls(CustClsConfig(deterministic=not train))
 
     init_fake_batch_size = 8
     input_shape = (init_fake_batch_size, 256)
@@ -45,48 +47,39 @@ class ModeEquivalenceTest(parameterized.TestCase):
 
     initial_params_original = orig_model.init({'params': rng},
                                               jnp.ones(input_shape, jnp.float32),
-                                              jnp.ones(target_shape, jnp.float32),
-                                              train=False)
+                                              jnp.ones(target_shape, jnp.float32))
     initial_params_custom = cust_model.init({'params': rng},
                                             jnp.ones(input_shape, jnp.float32),
-                                            jnp.ones(target_shape, jnp.float32),
-                                            train=False)
+                                            jnp.ones(target_shape, jnp.float32),)
 
     # fwd
 
-    for mode in ('train', 'eval'):
-      train = mode == 'train'
-      y1 = orig_model.apply(
-          initial_params_original,
-          jnp.ones(input_shape, jnp.float32),
-          jnp.ones(target_shape, jnp.float32),
-          train=train,
-          rngs={'dropout': dropout_rng},
-          mutable=['batch_stats'],)
-      y2 = cust_model.apply(
-          initial_params_custom,
-          jnp.ones(input_shape, jnp.float32),
-          jnp.ones(target_shape, jnp.float32),
-          train=train,
-          dropout_rate=dropout_rate,
-          rngs={'dropout': dropout_rng},
-          mutable=['batch_stats'])
+    y1 = orig_model.apply(
+        initial_params_original,
+        jnp.ones(input_shape, jnp.float32),
+        jnp.ones(target_shape, jnp.float32),
+        rngs={'dropout': dropout_rng})
 
-      for i in range(len(y1)):
-        assert jnp.allclose(y1[i], y2[i])
+    y2 = cust_model.apply(
+        initial_params_custom,
+        jnp.ones(input_shape, jnp.float32),
+        jnp.ones(target_shape, jnp.float32),
+        dropout_rate=dropout_rate,
+        rngs={'dropout': dropout_rng})
+
+    assert jnp.allclose(y1, y2)
 
 
 
   @parameterized.named_parameters(
-      dict(testcase_name='WMT, default'),
+      dict(testcase_name='WMT, default train', train=True),
+      dict(testcase_name='WMT, default eval', train=False),
   )
-  def test_default_dropout(self):
+  def test_default_dropout(self, train):
     """Test default dropout_rate."""
-    # init model
     rng, data_rng, dropout_rng = jax.random.split(jax.random.key(SEED), 3)
-
-    orig_model = OrigCls(OrigClsConfig)
-    cust_model = CustCls(CustClsConfig)
+    orig_model = OrigCls(OrigClsConfig(deterministic=not train))
+    cust_model = CustCls(CustClsConfig(deterministic=not train))
 
     init_fake_batch_size = 8
     input_shape = (init_fake_batch_size, 256)
@@ -94,35 +87,26 @@ class ModeEquivalenceTest(parameterized.TestCase):
 
     initial_params_original = orig_model.init({'params': rng},
                                               jnp.ones(input_shape, jnp.float32),
-                                              jnp.ones(target_shape, jnp.float32),
-                                              train=False)
+                                              jnp.ones(target_shape, jnp.float32))
     initial_params_custom = cust_model.init({'params': rng},
                                             jnp.ones(input_shape, jnp.float32),
-                                            jnp.ones(target_shape, jnp.float32),
-                                            train=False)
+                                            jnp.ones(target_shape, jnp.float32))
 
     # fwd
-    x = [jax.random.normal(data_rng, (2, *x)) for x in INPUT_SHAPE]
 
-    for mode in ('train', 'eval'):
-      train = mode == 'train'
-      y1 = orig_model.apply(
-          initial_params_original,
-          jnp.ones(input_shape, jnp.float32),
-          jnp.ones(target_shape, jnp.float32),
-          train=train,
-          rngs={'dropout': dropout_rng}, mutable=['batch_stats'])
-      y2 = cust_model.apply(
-          initial_params_custom,
-          jnp.ones(input_shape, jnp.float32),
-          jnp.ones(target_shape, jnp.float32),
-          train=train, rngs={'dropout': dropout_rng},
-          mutable=['batch_stats'])
+    y1 = orig_model.apply(
+        initial_params_original,
+        jnp.ones(input_shape, jnp.float32),
+        jnp.ones(target_shape, jnp.float32),
+        rngs={'dropout': dropout_rng})
 
-      print(jax.tree.map(lambda x: x.shape, y1))
-      
-      for i in range(len(y1)):
-        assert jnp.allclose(y1[i], y2[i])
+    y2 = cust_model.apply(
+        initial_params_custom,
+        jnp.ones(input_shape, jnp.float32),
+        jnp.ones(target_shape, jnp.float32),
+        rngs={'dropout': dropout_rng})
+        
+    assert jnp.allclose(y1, y2)
 
 
 if __name__ == '__main__':
