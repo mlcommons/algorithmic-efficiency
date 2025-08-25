f4c22685f110bf2d1f18255f94ef307caf5b1bc1
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2025-08-25 14:42:59 +0200

    dropout piping in submission: prize quali

--- reference_algorithms/qualification_baselines/external_tuning/jax_nadamw_full_budget.py
+++ reference_algorithms/qualification_baselines/external_tuning/jax_nadamw_full_budget.py
@@ -215,6 +215,7 @@ def train_step(
   rng,
   grad_clip,
   label_smoothing,
+  dropout_rate,
 ):
   def _loss_fn(params):
     """Loss function used for training."""
@@ -225,6 +226,7 @@ def train_step(
       spec.ForwardPassMode.TRAIN,
       rng,
       update_batch_norm=True,
+      dropout_rate=dropout_rate,
     )
     loss_dict = workload.loss_fn(
       label_batch=batch['targets'],
@@ -289,6 +291,10 @@ def update_params(
     grad_clip = hyperparameters.grad_clip
   else:
     grad_clip = None
+  if hasattr(hyperparameters, 'dropout_rate'):
+    dropout_rate = hyperparameters.dropout_rate
+  else:
+    dropout_rate = 0.0
 
   # Create shardings for each argument
   replicated = jax_sharding_utils.get_replicate_sharding()  # No partitioning
@@ -307,6 +313,7 @@ def update_params(
     replicated,  # rng
     replicated,  # grad_clip
     replicated,  # label_smoothing
+    replicated,  # dropout_rate
   )
   out_shardings = (
     replicated,  # new_optimizer_state
@@ -335,6 +342,7 @@ def update_params(
       rng,
       grad_clip,
       label_smoothing,
+      dropout_rate
     )
   )
 
