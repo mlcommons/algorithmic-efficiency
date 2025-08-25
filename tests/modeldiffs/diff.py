034e7a435ca3e9ca8e1e228599262fc0081f3477
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2025-08-25 15:25:21 +0200

    pipe dropout=0.0 to modeldiff

--- tests/modeldiffs/diff.py
+++ tests/modeldiffs/diff.py
@@ -13,12 +13,11 @@ def torch2jax(
   pytorch_workload,
   key_transform=None,
   sd_transform=None,
-  init_kwargs=dict(dropout_rate=0.0, aux_dropout_rate=0.0),
 ):
   jax_params, model_state = jax_workload.init_model_fn(
-    jax.random.PRNGKey(0), **init_kwargs
+    jax.random.PRNGKey(0)
   )
-  pytorch_model, _ = pytorch_workload.init_model_fn([0], **init_kwargs)
+  pytorch_model, _ = pytorch_workload.init_model_fn([0])
   if isinstance(jax_params, dict):
     jax_params = FrozenDict(jax_params)
   jax_params = jax_utils.unreplicate(jax_params).unfreeze()
